a8d7f5a0f6654316641850048df91a785ef86b45
okapetan <kapetan.ok@gmail.com>
2012-02-16 16:53:22 +0100

    Plus an algorithm to see graphical results

--- openpixi/src/org/openpixi/physics/Particle2D.java
+++ openpixi/src/org/openpixi/physics/Particle2D.java
@@ -11,8 +11,8 @@ public class Particle2D{
 	public double vx;           //velocity in the x - direction
 	public double vy;           //velocity in the y - direction
 	
-	public double ax;           //acceleration in the x - direction
-	public double ay;           //acceleration in the y - direction
+	private double ax;           //acceleration in the x - direction
+	private double ay;           //acceleration in the y - direction
 
 	private double mass;         // the mass of the particle
 	private double echarge;      //the electric charge of the particle  
@@ -76,8 +76,39 @@ public class Particle2D{
 		return Math.sqrt(range);
 	}
 	
+	/*this is a method, that  is based on the simple Euler-Richardson algorithm (it represents a neat way of finding the
+	 * numerical solutions of a differential equation, based on the Euler algorithm),
+	 *  where I have used the following literature:
+	 * http://www.physics.udel.edu/~bnikolic/teaching/phys660/numerical_ode/node4.html
+	 * I shall use this method togethet with the class Force.java to get some graphical results*/
+	public void algorithm(double step, Force f)
+	{
+		boolean onlyonce = true;
+		
+		if(onlyonce)
+		{
+			ax = f.getForceX(vx, vy, this) / mass;
+			ay = f.getForceY(vx, vy, this) / mass;
+			onlyonce = false;
+		}
+		//starting the Euler-Richardson algorithm (the equations correspond with the ones on the above mentioned website)
+		double vxmiddle = vx + ax * step / 2;
+		double vymiddle = vy + ay * step / 2;
+		
+		//double xmiddle = x + vx * step / 2;    actually, this two equations are not needed, but I've written them
+		//double ymiddle = y + vy * step / 2;    so the algorithm is complete
+		
+		double axmiddle = f.getForceX(vxmiddle, vymiddle, this) / mass;
+		double aymiddle = f.getForceY(vxmiddle, vymiddle, this) / mass;
+		
+		vx += axmiddle * step;
+		vy += aymiddle * step;
+		
+		x += vxmiddle * step;
+		y += vymiddle * step;
+		
+		ax = f.getForceX(vx, vy, this) / mass;
+		ay = f.getForceY(vx, vy, this) / mass;
+	}
+	
 }
-
-
-
-
