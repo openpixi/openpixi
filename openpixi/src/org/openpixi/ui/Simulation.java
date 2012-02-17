44412f430cd82999dda7ceeec7211980a79ea04d
KaPeTaN <KaPeTaN@KaPeTaN-PC.lan>
2012-02-17 16:51:17 +0100

    Adding the boundaries

--- openpixi/src/org/openpixi/ui/Simulation.java
+++ openpixi/src/org/openpixi/ui/Simulation.java
@@ -6,39 +6,39 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Simulation extends JFrame {
-	
+
 	public Simulation()
 	{
 		setVisible(true);
 		setSize(700, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	private void path(Graphics graph)
 	{
 		final double stepmax = 15;      //how many steps will the simulation last
 		final double step = 0.5;     //the step that is used in the algorithm
-		
+
 		Particle2D par = new Particle2D(0.0, 400.0, 30.0, 30.0, 0.0, 0.0, 1, 0);   //The particle
 		Force f = new Force(0.0);                                                  //The Force with 0 dragging
-		
+		par.setBoundaries(getHeight(), getWidth());
 		graph.setColor(Color.blue);                                              //setting the color of the graphics
-		
+
 		for(double t = 0.0; t < stepmax; t += step)                                      //drawing the path
 		{
 			graph.fillOval((int)par.x, (int)par.y, 15, 15);
 			par.algorithm(step, f);
 		}
 	}
-	
+
 	public void paint(Graphics graph)
 	{
 		setBackground(Color.white);
 		path(graph);
 	}
-	
+
 	public static void main(String[] args){
-		
+
 		Simulation sim = new Simulation();                                //starts the simulation
 	}
 
