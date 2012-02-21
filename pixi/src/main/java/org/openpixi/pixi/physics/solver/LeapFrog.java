231a76e0262d3e98b150386e5f3ea09605db6bb8
okapetan <kapetan.ok@gmail.com>
2012-02-22 00:13:39 +0100

    Optimizing the algorithm

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrog.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrog.java
@@ -1,5 +1,10 @@
 /*The algorithm and the equations that are used can be find here:
  * http://phycomp.technion.ac.il/~david/thesis/node34.html
+ * and also here:
+ * http://www.artcompsci.org/vol_1/v1_web/node34.html#leapfrog-step2
+ * The first equations that are as comments now are just a different way of writing the algorithm.
+ * The other equations that are used now one can find in the second link.
+ * Personally I find the second way better, but it is just a question of taste.
  */
 
 package org.openpixi.pixi.physics.solver;
@@ -9,7 +14,7 @@ import org.openpixi.pixi.physics.*;
 public class LeapFrog {
 	public static void algorithm(Particle2D particle, Force f, double step) {
 		
-		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		/*particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
 		particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
 		
 		double vxminushalf = particle.vx;
@@ -25,7 +30,19 @@ public class LeapFrog {
 		particle.y += vyplushalf * step;
 		
 		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;*/
+		
+		particle.vx += particle.ax * step / 2;
+		particle.vy += particle.ay * step / 2;
+		
+		particle.x += particle.vx * step;
+		particle.y += particle.vy * step;
+		
+		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
 		particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		
+		particle.vx += particle.ax * step / 2;
+		particle.vy += particle.ay * step / 2;
 	}
 
 }
