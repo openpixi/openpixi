60855f57b6cafb643bb551399e960342472f98e8
okapetan <kapetan.ok@gmail.com>
2012-02-21 23:15:00 +0100

    Basic code for the LeapFrog - Algorithm, it will be updated

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrog.java
@@ -0,0 +1,27 @@
+package org.openpixi.pixi.physics.solver;
+
+import org.openpixi.pixi.physics.*;
+
+public class LeapFrog {
+	public static void algorithm(Particle2D particle, Force f, double step) {
+		
+		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		
+		double vxminushalf = particle.vx;
+		double vyminushalf = particle.vy;
+		
+		double vxplushalf = vxminushalf + particle.ax * step;;
+		double vyplushalf = vyminushalf + particle.ay * step;;
+		
+		particle.vx = (vxplushalf);// - vxminushalf) / (double) 2;
+		particle.vy = (vyplushalf);// - vyminushalf) / (double) 2;
+		
+		particle.x += vxplushalf * step;
+		particle.y += vyplushalf * step;
+		
+		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+	}
+
+}
