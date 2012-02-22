203323849cf092fc288005acd61a3a521910700c
okapetan <kapetan.ok@gmail.com>
2012-02-22 18:03:48 +0100

    The Euler algorithm

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
@@ -0,0 +1,22 @@
+package org.openpixi.pixi.physics.solver;
+
+import org.openpixi.pixi.physics.*;
+
+
+public class Euler {
+	public static void algorithm(Particle2D particle, Force f, double step)
+	{
+		//particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		//particle.ay = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		
+		particle.vx += particle.ax * step;
+		particle.vy += particle.ay * step;
+		
+		particle.x += particle.vx * step;
+		particle.y += particle.vy * step;
+		
+		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
+	}
+
+}
