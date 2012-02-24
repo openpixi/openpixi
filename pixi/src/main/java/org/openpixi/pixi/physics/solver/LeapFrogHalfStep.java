d31ee168702595ef658e96ea89d32b526bbce110
okapetan <kapetan.ok@gmail.com>
2012-02-24 17:10:04 +0100

    Diving the algorithms from LeapFrog.java

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrogHalfStep.java
@@ -0,0 +1,26 @@
+package org.openpixi.pixi.physics.solver;
+
+import org.openpixi.pixi.physics.Force;
+import org.openpixi.pixi.physics.Particle2D;
+
+public class LeapFrogHalfStep {
+	public static void algorithm(Particle2D p, Force f, double dt) {
+		// v(t+dt/2) = v(t) + a(t)*dt/2
+		p.vx += p.ax * dt / 2.0;
+		p.vy += p.ay * dt / 2.0;
+
+		// x(t+dt) = x(t) + v(t+dt/2)*dt
+		p.x += p.vx * dt;
+		p.y += p.vy * dt;
+
+		// a(t+dt) = F(v(t+dt/2), x(t+dt)) / m
+		// WARNING: Force is evaluated at two different times t+dt/2 and t+dt!
+		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
+		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
+
+		// v(t+dt) = v(t+dt/2) + a(t+dt)*dt/2
+		p.vx += p.ax * dt / 2.0;
+		p.vy += p.ay * dt / 2.0;
+	}
+
+}
