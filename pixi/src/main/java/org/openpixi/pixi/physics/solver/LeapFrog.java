076c707f305c8f9179001f1b74ffd0b354348d81
Andreas Ipp <andreas.ipp@gmail.com>
2012-02-24 14:58:16 +0100

    Implement correct LeapFrog algorithm and add comments.

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrog.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrog.java
@@ -12,39 +12,50 @@ package org.openpixi.pixi.physics.solver;
 import org.openpixi.pixi.physics.*;
 
 public class LeapFrog {
+
+	/**
+	 * LeapFrog algorithm.
+	 * Warning: the velocity is stored half a time step ahead of the position.
+	 * @param particle before the update: x(t), v(t+dt/2), a(t);
+	 *                 after the update: x(t+dt), v(t+3*dt/2), a(t+dt)
+	 */
 	public static void algorithm(Particle2D particle, Force f, double step) {
-		
-		boolean onlyonce = true;
-		//particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
-		//particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
-		
-		double vxminushalf = particle.vx;
-		double vyminushalf = particle.vy;
-
-		double vxplushalf = vxminushalf + particle.ax * step;;
-		double vyplushalf = vyminushalf + particle.ay * step;;
-
-		particle.x += vxplushalf * step;
-		particle.y += vyplushalf * step;
-
-		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
-		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
-
-		particle.vx = (vxplushalf + vxminushalf) / 2.0;
-		particle.vy = (vyplushalf + vyminushalf) / 2.0;	
-		
-	}
-	public static void algorithmHalfStep(Particle2D particle, Force f, double step) {
-		
-		particle.vx += particle.ax * step / 2.0;
-		particle.vy += particle.ay * step / 2.0;
-		
+		// x(t+dt) = x(t) + v(t+dt/2)*dt
 		particle.x += particle.vx * step;
 		particle.y += particle.vy * step;
-		
+
+		// a(t+dt) = F(v(t+dt/2), x(t+dt)) / m
+		// WARNING: Force is evaluated at two different times t+dt/2 and t+dt!
 		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
 		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
+
+		// v(t+3*dt/2) = v(t+dt/2) + a(t+dt)*dt
+		particle.vx += particle.ax * step;
+		particle.vy += particle.ay * step;
 		
+	}
+
+	/**
+	 * LeapFrog algorithm.
+	 * The velocity is stored at the same times as the position.
+	 * @param particle before the update: x(t), v(t), a(t);
+	 *                 after the update: x(t+dt), v(t+dt), a(t+dt)
+	 */
+	public static void algorithmHalfStep(Particle2D particle, Force f, double step) {
+		// v(t+dt/2) = v(t) + a(t)*dt/2
+		particle.vx += particle.ax * step / 2.0;
+		particle.vy += particle.ay * step / 2.0;
+
+		// x(t+dt) = x(t) + v(t+dt/2)*dt
+		particle.x += particle.vx * step;
+		particle.y += particle.vy * step;
+
+		// a(t+dt) = F(v(t+dt/2), x(t+dt)) / m
+		// WARNING: Force is evaluated at two different times t+dt/2 and t+dt!
+		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
+		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
+
+		// v(t+dt) = v(t+dt/2) + a(t+dt)*dt/2
 		particle.vx += particle.ax * step / 2.0;
 		particle.vy += particle.ay * step / 2.0;
 	}
