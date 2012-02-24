73c70b6c520fbdfd860473b2c205b263437c873f
okapetan <kapetan.ok@gmail.com>
2012-02-24 16:18:37 +0100

    The Euler algorithm

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
@@ -10,14 +10,13 @@ public class Euler extends Solver {
 		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
 		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
 		
-		// v(t+dt) = v(t) + a(t)*dt
-		p.vx += p.ax * step;
-		p.vy += p.ay * step;
-		
-		// x(t+dt) = x(t) + v(t+dt)*dt
+		// x(t+dt) = x(t) + v(t)*dt
 		p.x += p.vx * step;
 		p.y += p.vy * step;
 		
+		// v(t+dt) = v(t) + a(t)*dt
+		p.vx += p.ax * step;
+		p.vy += p.ay * step;
 	}
 
 }
