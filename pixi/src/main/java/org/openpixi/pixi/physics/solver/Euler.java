737e704c1500143022d33455488c83c293605ae1
okapetan <kapetan.ok@gmail.com>
2012-02-24 16:02:14 +0100

    Adding comments

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
@@ -6,12 +6,15 @@ import org.openpixi.pixi.physics.*;
 public class Euler extends Solver {
 	public static void algorithm(Particle2D particle, Force f, double step)
 	{
+		//a(t) = F(v(t), x(t)) / m
 		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
 		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
 		
+		// v(t+dt) = v(t) + a(t)*dt
 		particle.vx += particle.ax * step;
 		particle.vy += particle.ay * step;
 		
+		// x(t+dt) = x(t) + v(t+dt)*dt
 		particle.x += particle.vx * step;
 		particle.y += particle.vy * step;
 		
