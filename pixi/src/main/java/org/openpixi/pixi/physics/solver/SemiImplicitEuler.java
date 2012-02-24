e9a9642eac623866a9caeadebafc5a7a969465bd
okapetan <kapetan.ok@gmail.com>
2012-02-24 16:16:36 +0100

    Renaming

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/SemiImplicitEuler.java
@@ -0,0 +1,23 @@
+package org.openpixi.pixi.physics.solver;
+
+import org.openpixi.pixi.physics.*;
+
+
+public class SemiImplicitEuler extends Solver {
+	public static void algorithm(Particle2D p, Force f, double step)
+	{
+		//a(t) = F(v(t), x(t)) / m
+		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
+		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
+		
+		// v(t+dt) = v(t) + a(t)*dt
+		p.vx += p.ax * step;
+		p.vy += p.ay * step;
+		
+		// x(t+dt) = x(t) + v(t+dt)*dt
+		p.x += p.vx * step;
+		p.y += p.vy * step;
+		
+	}
+
+}
