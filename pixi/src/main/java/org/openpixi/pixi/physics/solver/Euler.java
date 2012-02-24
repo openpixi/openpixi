0084010cd76a8e58c64298056ec1e508b53b58f8
okapetan <kapetan.ok@gmail.com>
2012-02-24 16:03:39 +0100

    Using shorter names for the variables

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/Euler.java
@@ -4,19 +4,19 @@ import org.openpixi.pixi.physics.*;
 
 
 public class Euler extends Solver {
-	public static void algorithm(Particle2D particle, Force f, double step)
+	public static void algorithm(Particle2D p, Force f, double step)
 	{
 		//a(t) = F(v(t), x(t)) / m
-		particle.ax = f.getForceX(particle.vx, particle.vy, particle) / particle.mass;
-		particle.ay = f.getForceY(particle.vx, particle.vy, particle) / particle.mass;
+		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
+		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
 		
 		// v(t+dt) = v(t) + a(t)*dt
-		particle.vx += particle.ax * step;
-		particle.vy += particle.ay * step;
+		p.vx += p.ax * step;
+		p.vy += p.ay * step;
 		
 		// x(t+dt) = x(t) + v(t+dt)*dt
-		particle.x += particle.vx * step;
-		particle.y += particle.vy * step;
+		p.x += p.vx * step;
+		p.y += p.vy * step;
 		
 	}
 
