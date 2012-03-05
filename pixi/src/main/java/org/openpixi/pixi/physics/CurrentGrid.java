07171c7672b7e8d3e1da4e3506618063a5e9b5c2
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-05 18:13:18 +0100

    Changing privat to public for some variables

--- pixi/src/main/java/org/openpixi/pixi/physics/CurrentGrid.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/CurrentGrid.java
@@ -1,3 +1,21 @@
+/*
+ * OpenPixi - Open Particle-In-Cell (PIC) Simulator
+ * Copyright (C) 2012  OpenPixi.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openpixi.pixi.physics;
 
 import java.util.ArrayList;
@@ -11,8 +29,8 @@ public class CurrentGrid {
 	public static int X_BOX = 10;
 	public static int Y_BOX = 10;
 	
-	private double cellWidth;
-	private double cellHeight;
+	public double cellWidth;
+	public double cellHeight;
 	
 	//the constructor
 	public CurrentGrid(int panelWidth, int panelHeight) {
@@ -48,6 +66,7 @@ public class CurrentGrid {
 			jx[xCellPosition][yCellPosition] += par.charge * par.vx;
 			jy[xCellPosition][yCellPosition] += par.charge * par.vy;
 		}
+		
 	}
 
 }
