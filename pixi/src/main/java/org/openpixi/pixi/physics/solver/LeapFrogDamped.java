fe0a2ec9496f932f82253bf965e9ca0f3dc5d1c7
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-08 12:47:24 +0100

    Implementing the dragging

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrogDamped.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/LeapFrogDamped.java
@@ -25,37 +25,50 @@ public class LeapFrogDamped extends Solver{
 	 *                 after the update: x(t+dt), v(t+3*dt/2), a(t+dt)
 	 */
 	public void step(Particle2D p, Force f, double dt) {
+		
+		//help coefficients for the dragging
+		double help1_coef = 1 - f.drag * dt / (2 * p.mass);
+		double help2_coef = 1 + f.drag * dt / (2 * p.mass);
+		
 		// x(t+dt) = x(t) + v(t+dt/2)*dt
 		p.x += p.vx * dt;
 		p.y += p.vy * dt;
 
 		// a(t+dt) = F(v(t+dt/2), x(t+dt)) / m
 		// WARNING: Force is evaluated at two different times t+dt/2 and t+dt!
-		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
-		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
+		p.ax = (f.getPositionComponentofForceX(p) + f.getNormalVelocityComponentofForceX(p)) / p.mass;
+		p.ay = (f.getPositionComponentofForceY(p) + f.getNormalVelocityComponentofForceY(p)) / p.mass;
+
 
 		// v(t+3*dt/2) = v(t+dt/2) + a(t+dt)*dt
-		p.vx += p.ax * dt;
-		p.vy += p.ay * dt;
+		p.vx = (p.vx * help1_coef + p.ax * dt) / help2_coef;
+		p.vy = (p.vy * help1_coef + p.ay * dt) / help2_coef;
 		
 	}
 	
 	public void prepare(Particle2D p, Force f, double dt)
 	{
+		double help1_coef = 1 - f.drag * dt / (2 * p.mass);
+		double help2_coef = 1 + f.drag * dt / (2 * p.mass);
+		
 		//a(t) = F(v(t), x(t)) / m
-		p.ax = f.getForceX(p.vx, p.vy, p) / p.mass;
-		p.ay = f.getForceY(p.vx, p.vy, p) / p.mass;
+		p.ax = (f.getPositionComponentofForceX(p) + f.getNormalVelocityComponentofForceX(p)) / p.mass;
+		p.ay = (f.getPositionComponentofForceY(p) + f.getNormalVelocityComponentofForceY(p)) / p.mass;
+
 		
 		//v(t + dt / 2) = v(t) + a(t)*dt / 2
-		p.vx += p.ax * dt / 2;
-		p.vy += p.ay * dt / 2;
+		p.vx = (p.vx * help1_coef + p.ax * dt * 0.5) / help2_coef;
+		p.vy = (p.vy * help1_coef + p.ay * dt * 0.5) / help2_coef;
 	}
 	
 	public void complete(Particle2D p, Force f, double dt)
 	{
+		double help1_coef = 1 - f.drag * dt / (2 * p.mass);
+		double help2_coef = 1 + f.drag * dt / (2 * p.mass);
+		
 		//v(t) = v(t + dt / 2) - a(t)*dt / 2
-		p.vx -= p.ax * dt / 2;
-		p.vy -= p.ay * dt / 2;
+		p.vx = (p.vx * help2_coef - p.ax * dt * 0.5) / help1_coef;
+		p.vy = (p.vy * help2_coef - p.ay * dt * 0.5) / help1_coef;
 	}
 
 }
