338671497c546f9b3532306137a92bc19902380c
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-03-14 16:02:54 +0100

    Moving FieldSolver class to fields package

--- pixi/src/main/java/org/openpixi/pixi/physics/fields/FieldSolver.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/FieldSolver.java
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package org.openpixi.pixi.physics;
+package org.openpixi.pixi.physics.fields;
 
 public abstract class FieldSolver {
 	
