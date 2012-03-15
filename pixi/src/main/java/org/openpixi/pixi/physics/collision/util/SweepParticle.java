acd7b95eec74363be173f2d876233626f6b0b6b0
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 14:03:32 +0100

    Adding the contructor

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/SweepParticle.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/SweepParticle.java
@@ -1,5 +1,19 @@
 package org.openpixi.pixi.physics.collision.util;
 
+import org.openpixi.pixi.physics.*;
+
 public class SweepParticle {
+	
+	public Particle2D particle;
+	public final int axis;
+	public boolean begin;
+	public double value;
+	
+	public SweepParticle(Particle2D particle, int axis, boolean begin) {
+		
+		this.particle = particle;
+		this.axis = axis;
+		this.begin = begin;
+	}
 
 }
