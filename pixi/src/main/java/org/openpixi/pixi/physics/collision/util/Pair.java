a021ff40141f3c5876c8007e0d9c05575d421fb3
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 13:17:47 +0100

    Adding an equals method

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
@@ -15,7 +15,21 @@ public class Pair<A, B> {
 		int hashSecond = second != null ? second.hashCode() : 0;
 		
 		return (hashFirst + hashSecond) * hashFirst + hashSecond;
-		
 	}
+	
+	public boolean equals (Object another) {
+		if(another instanceof Pair) {
+			Pair anotherPair = (Pair) another;
+			return
+					((this.first == anotherPair.first || (this.first != null && anotherPair.first != null && 
+					this.first.equals(anotherPair.first))) &&
+					(this.second == anotherPair.second || (this.second != null && anotherPair.second != null &&
+					this.second.equals(anotherPair.second))));
+		}
+		
+		return false;
+	}
+	
+	
 	
 }
