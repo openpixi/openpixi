67e19702a294fd7231abae976511bbd945178405
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 13:08:59 +0100

    Adding a hashcode methode

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
@@ -1,5 +1,21 @@
 package org.openpixi.pixi.physics.collision.util;
 
-public class Pair {
+public class Pair<A, B> {
+	
+	private A first;
+	private B second;
+	
+	public Pair(A first, B second) {
+		this.first = first;
+		this.second = second;
+	}
 
+	public int hashCode() {
+		int hashFirst = first != null ? first.hashCode() : 0;
+		int hashSecond = second != null ? second.hashCode() : 0;
+		
+		return (hashFirst + hashSecond) * hashFirst + hashSecond;
+		
+	}
+	
 }
