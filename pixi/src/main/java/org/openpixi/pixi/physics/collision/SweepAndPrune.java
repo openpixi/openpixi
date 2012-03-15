b25dca5eb060ed5462e936daca2c4d69b8a3df42
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 19:46:43 +0100

    Changing lists to arrays

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/SweepAndPrune.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/SweepAndPrune.java
@@ -10,24 +10,25 @@ import java.util.Set;
 public class SweepAndPrune {
 	
 	private final int MAX_PARTICLES = 10;
-	//private ArrayList<SweepParticle> axisX = new ArrayList<SweepParticle>(MAX_PARTICLES);
-	private SweepParticle [] axisX = new SweepParticle[MAX_PARTICLES];
-	//private ArrayList<SweepParticle> axisY = new ArrayList<SweepParticle>(MAX_PARTICLES);
-	SweepParticle [] axisY = new SweepParticle[MAX_PARTICLES];
-	private Set<Pair<BoundingBox, BoundingBox>> overlaps = new LinkedHashSet<Pair<BoundingBox, BoundingBox>>();
+	private ArrayList<BoundingBox> boxlist = new ArrayList<BoundingBox>();
+	private ArrayList<SweepParticle> axisX = new ArrayList<SweepParticle>();
+	//private SweepParticle [] axisX = new SweepParticle[MAX_PARTICLES];
+	private ArrayList<SweepParticle> axisY = new ArrayList<SweepParticle>();
+	//SweepParticle [] axisY = new SweepParticle[MAX_PARTICLES];
+	private ArrayList<Pair<BoundingBox, BoundingBox>> overlaps = new ArrayList<Pair<BoundingBox, BoundingBox>>();
 	private Map<Pair<BoundingBox, BoundingBox>, Integer> overlapCounter = new LinkedHashMap<Pair<BoundingBox, BoundingBox>, Integer>();
 	
 	public SweepAndPrune() {
 		
 	}
 	
-	int i = 0;
 	public void add(BoundingBox box) {
-		axisX[i * 2] = new SweepParticle(box, 0, true);
-		axisX[i * 2 + 1] = new SweepParticle(box, 0, false);
-		axisY[i * 2] = new SweepParticle(box, 1, true);
-		axisY[i * 2 + 1] = new SweepParticle(box, 1, false);
-		i++;
+		if(!boxlist.contains(box)) {
+			axisX.add(new SweepParticle(box, 0, true));
+			axisX.add(new SweepParticle(box, 0, false));
+			axisY.add(new SweepParticle(box, 1, true));
+			axisY.add(new SweepParticle(box, 1, false));
+		}
 	}
 
 }
