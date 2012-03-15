db039cb7b8e2629f95f11cf7c87a09dbff71722c
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 19:13:38 +0100

    Adding an add method for the sweep particles

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/SweepAndPrune.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/SweepAndPrune.java
@@ -10,10 +10,10 @@ import java.util.Set;
 public class SweepAndPrune {
 	
 	private final int MAX_PARTICLES = 10;
-	//private ArrayList<BoundingBox> axisX = new ArrayList<BoundingBox>(MAX_PARTICLES);
-	private BoundingBox [] axisX = new BoundingBox[MAX_PARTICLES];
-	//private ArrayList<BoundingBox> axisY = new ArrayList<BoundingBox>(MAX_PARTICLES);
-	BoundingBox [] axisY = new BoundingBox[MAX_PARTICLES];
+	//private ArrayList<SweepParticle> axisX = new ArrayList<SweepParticle>(MAX_PARTICLES);
+	private SweepParticle [] axisX = new SweepParticle[MAX_PARTICLES];
+	//private ArrayList<SweepParticle> axisY = new ArrayList<SweepParticle>(MAX_PARTICLES);
+	SweepParticle [] axisY = new SweepParticle[MAX_PARTICLES];
 	private Set<Pair<BoundingBox, BoundingBox>> overlaps = new LinkedHashSet<Pair<BoundingBox, BoundingBox>>();
 	private Map<Pair<BoundingBox, BoundingBox>, Integer> overlapCounter = new LinkedHashMap<Pair<BoundingBox, BoundingBox>, Integer>();
 	
@@ -21,8 +21,13 @@ public class SweepAndPrune {
 		
 	}
 	
+	int i = 0;
 	public void add(BoundingBox box) {
-		
+		axisX[i * 2] = new SweepParticle(box, 0, true);
+		axisX[i * 2 + 1] = new SweepParticle(box, 0, false);
+		axisY[i * 2] = new SweepParticle(box, 1, true);
+		axisY[i * 2 + 1] = new SweepParticle(box, 1, false);
+		i++;
 	}
 
 }
