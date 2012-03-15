c9eb03a31e737a6b9fc08bb098a556319ac6fd9e
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 17:15:29 +0100

    Adding a method for getting & updating the boundaries / values

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/SweepParticle.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/SweepParticle.java
@@ -4,16 +4,37 @@ import org.openpixi.pixi.physics.*;
 
 public class SweepParticle {
 	
+	public BoundingBox bb;
 	public Particle2D particle;
 	public final int axis;
 	public boolean begin;
 	public double value;
 	
-	public SweepParticle(Particle2D particle, int axis, boolean begin) {
+	public SweepParticle(BoundingBox bb, Particle2D particle, int axis, boolean begin) {
 		
+		this.bb = bb;
 		this.particle = particle;
 		this.axis = axis;
 		this.begin = begin;
+	}
+	
+	public double updateGetValue() {
+		if(this.begin) {
+			if(this.axis == 0) {
+				return bb.xMin;
+			}
+			else {
+				return bb.yMin;
+			}
+		}
+		else {
+			if(this.axis == 0) {
+				return bb.xMax;
+			}
+			else {
+				return bb.yMax;
+			}
+		}
 	}
 
 }
