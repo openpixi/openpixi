1a90c092bed9069efa11c0014e89cdf189bf6fa5
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-15 20:39:39 +0100

    A class that counts the overlaps for the Sweet & Prune algorithm

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/OverlapCounter.java
@@ -0,0 +1,12 @@
+package org.openpixi.pixi.physics.collision.util;
+
+public class OverlapCounter {
+	
+	private boolean overLaping = false;
+	private int overlaps = 0;
+	
+	public OverlapCounter() {
+		
+	}
+
+}
