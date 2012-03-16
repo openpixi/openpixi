347a8079b014a06322e124901893463286c14d2f
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-16 20:51:38 +0100

    Commenting unnecessary code

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllListParticles.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllListParticles.java
@@ -32,13 +32,13 @@ public class AllListParticles extends Detector{
 			for(int k = (i + 1); k < particlelist.size(); k++)
 			{
 				Particle2D p2 = (Particle2D) particlelist.get(k);
-				double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-				if(distance <= (p1.radius + p2.radius)) {
+				//double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+				//if(distance <= (p1.radius + p2.radius)) {
 					
 					Pair<Particle2D, Particle2D> pair = new Pair<Particle2D, Particle2D>(p1, p2);
 					overlappedPairs.add(pair);
 					
-				}
+				//}
 			}
 			
 		}
