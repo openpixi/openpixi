68702b3f557d1b0538fabb41b67dfed3157dd60e
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-16 17:18:00 +0100

    Making an abstract basis class for the detectors

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/Detector.java
@@ -0,0 +1,13 @@
+package org.openpixi.pixi.physics.collision.detectors;
+
+import java.util.ArrayList;
+
+import org.openpixi.pixi.physics.Particle2D;
+
+public abstract class Detector {
+	
+	public Detector() {
+		
+	}
+
+}
