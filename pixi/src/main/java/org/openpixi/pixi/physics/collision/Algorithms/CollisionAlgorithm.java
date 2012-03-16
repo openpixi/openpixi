94e2537bebc10cfa2393ce1f2ae703d9a67695a5
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-16 17:41:26 +0100

    Adding a method doCollision

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/Algorithms/CollisionAlgorithm.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/Algorithms/CollisionAlgorithm.java
@@ -1,5 +1,18 @@
 package org.openpixi.pixi.physics.collision.Algorithms;
 
+import java.util.ArrayList;
+
+import org.openpixi.pixi.physics.Particle2D;
+import org.openpixi.pixi.physics.collision.util.Pair;
+
 public abstract class CollisionAlgorithm {
+	
+	public CollisionAlgorithm() {
+		
+	}
+	
+	public void doCollision(ArrayList<Pair<Particle2D, Particle2D>> pairs) {
+		
+	}
 
 }
