8f15ce4ddc8d8c78fa469a2cef2335cd91e01455
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-16 17:32:50 +0100

    Adding methods for output and run

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllListParticles.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllListParticles.java
@@ -3,8 +3,11 @@ package org.openpixi.pixi.physics.collision.detectors;
 import java.util.ArrayList;
 import org.openpixi.pixi.physics.*;
 import org.openpixi.pixi.physics.collision.util.BoundingBox;
+import org.openpixi.pixi.physics.collision.util.Pair;
 
 public class AllListParticles extends Detector{
+	
+	private ArrayList<Pair<Particle2D, Particle2D>> overlappedPairs = new ArrayList<Pair<Particle2D, Particle2D>>();
 	
 	private ArrayList<Particle2D> particlelist = new ArrayList<Particle2D>();
 	
@@ -19,6 +22,31 @@ public class AllListParticles extends Detector{
 			Particle2D par = (Particle2D) parlist.get(i);
 			this.particlelist.add(par);
 		}
+	}
+	
+	public void run() {
+		for(int i = 0; i < (particlelist.size() - 1); i++)
+		{
+			Particle2D p1 = (Particle2D) particlelist.get(i);
+			//double x1 = Math.sqrt(p1.x * p1.x + p1.y * p1.y);
+			for(int k = (i + 1); k < particlelist.size(); k++)
+			{
+				Particle2D p2 = (Particle2D) particlelist.get(k);
+				double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+				if(distance <= (p1.radius + p2.radius)) {
+					
+					Pair<Particle2D, Particle2D> pair = new Pair<Particle2D, Particle2D>(p1, p2);
+					overlappedPairs.add(pair);
+					
+				}
+			}
+			
+		}
+	}
+	
+	public ArrayList<Pair<Particle2D, Particle2D>> getOverlappedPairs() {
+		
+		return overlappedPairs;
 	}
 
 }
