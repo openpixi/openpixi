1e98073560c926700805b62bad0c678ab7a03df6
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-17 13:16:59 +0100

    Changing the constructor

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/ElasticCollision.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/ElasticCollision.java
@@ -18,8 +18,8 @@ import java.util.ArrayList;
 
 public class ElasticCollision extends Collision{
 	
-	public ElasticCollision(Detector det, CollisionAlgorithm alg) {
-		super(det, alg);
+	public ElasticCollision() {
+		super();
 	}
 	
 	/*public void doCollision(Particle2D p1, Particle2D p2)
