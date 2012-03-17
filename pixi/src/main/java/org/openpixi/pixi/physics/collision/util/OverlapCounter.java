c86393c618c61df6525fe732eae8659e9aa81921
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-17 18:12:13 +0100

    Changing a name of a variable

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/OverlapCounter.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/OverlapCounter.java
@@ -2,7 +2,7 @@ package org.openpixi.pixi.physics.collision.util;
 
 public class OverlapCounter {
 	
-	public boolean overlapping = false;
+	public boolean overlappedBoolean = false;
 	public int overlaps = 0;
 	
 	public OverlapCounter() {
