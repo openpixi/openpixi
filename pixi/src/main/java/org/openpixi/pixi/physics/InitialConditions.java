37e8805d25af4d1d0543d8d2d6134163ebe4c2f7
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-17 15:38:47 +0100

    Commenting code

--- pixi/src/main/java/org/openpixi/pixi/physics/InitialConditions.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/InitialConditions.java
@@ -85,6 +85,7 @@ public class InitialConditions {
 
 	public static void createRandomParticles(Simulation s, int count, double radius) {
 		s.particles.clear();
+		//s.detector.reset();
 		for (int k = 0; k < count; k++) {
 			Particle2D par = new Particle2D();
 			par.x = s.width * Math.random();
@@ -100,6 +101,7 @@ public class InitialConditions {
 			}
 			s.particles.add(par);
 		}
+		//s.detector.add(s.particles);
 	}
 
 	public static void setHardWallBoundary(Simulation s) {
