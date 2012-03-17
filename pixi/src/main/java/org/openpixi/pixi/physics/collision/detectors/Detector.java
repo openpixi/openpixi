1612489b0502e673543ecebb9f6075aa50d7317e
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-17 13:29:37 +0100

    Changing the class into not abstract

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/Detector.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/Detector.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import org.openpixi.pixi.physics.Particle2D;
 import org.openpixi.pixi.physics.collision.util.Pair;
 
-public abstract class Detector {
+public class Detector {
 	
 	public ArrayList<Pair<Particle2D, Particle2D>> overlappedPairs = new ArrayList<Pair<Particle2D, Particle2D>>();
 	
