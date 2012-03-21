1de11f8569e0bae1ad23289b680afdad828ccdc0
Andreas Ipp <andreas.ipp@gmail.com>
2012-03-21 14:08:04 +0100

    add new class CombinedForce

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/force/CombinedForce.java
@@ -0,0 +1,108 @@
+package org.openpixi.pixi.physics.force;
+
+import java.util.ArrayList;
+
+import org.openpixi.pixi.physics.Particle2D;
+import org.openpixi.pixi.physics.Simulation;
+
+/**
+ * Combines various forces into a single force.
+ */
+public class CombinedForce extends Force {
+
+	ArrayList<Force> forces = new ArrayList<Force>();
+
+	public CombinedForce(Simulation s) {
+		super(s);
+	}
+
+	/**
+	 * Adds another force.
+	 * @param f
+	 */
+	public void add(Force force) {
+		forces.add(force);
+	}
+
+	@Override
+	public double getForceX(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getForceX(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getForceY(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getForceY(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getPositionComponentofForceX(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getPositionComponentofForceX(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getPositionComponentofForceY(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getPositionComponentofForceY(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getTangentVelocityComponentOfForceX(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getTangentVelocityComponentOfForceX(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getTangentVelocityComponentOfForceY(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getTangentVelocityComponentOfForceY(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getNormalVelocityComponentofForceX(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getNormalVelocityComponentofForceX(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getNormalVelocityComponentofForceY(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getNormalVelocityComponentofForceY(p);
+		}
+		return sum;
+	}
+
+	@Override
+	public double getB(Particle2D p) {
+		double sum = 0;
+		for (Force f : forces) {
+			sum += f.getB(p);
+		}
+		return sum;
+	}
+
+}
