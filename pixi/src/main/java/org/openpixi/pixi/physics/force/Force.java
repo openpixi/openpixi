60e30083efaca4254758eca5e97c3ae42f671432
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-03-21 09:30:03 +0100

    added force from grid to particle

--- pixi/src/main/java/org/openpixi/pixi/physics/force/Force.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/force/Force.java
@@ -19,7 +19,10 @@
 //First I would like to start with very simple class Force.java, so we could see the graphic result.
 package org.openpixi.pixi.physics.force;
 
+import java.util.ArrayList;
+
 import org.openpixi.pixi.physics.Particle2D;
+import org.openpixi.pixi.physics.Simulation;
 
 public class Force {
 
@@ -41,9 +44,12 @@ public class Force {
 	/** Magnetic field in z - direction */
 	public double bz;
 	
+	Simulation s;
+	
 	/** New empty force */
-	public Force()
+	public Force(Simulation s)
 	{
+		this.s = s;
 		reset();
 	}
 
@@ -58,44 +64,66 @@ public class Force {
 	}
 	
 	//getting the force in the x - direction
-	public double getForceX(Particle2D par)
-	{
-		return -drag * par.vx + par.mass * gx + par.charge * ex + par.charge * par.vy * bz;
+	public double getForceX(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return -drag * par.vx + par.mass * gx + par.charge * (ex + s.currentGrid.Ex[xCellPosition][yCellPosition]) +
+				par.charge * par.vy * (bz + s.currentGrid.Bz[xCellPosition][yCellPosition]);
 	}
 	
 	//getting the force in the y - direction
-	public double getForceY(Particle2D par)
-	{
-		return - drag * par.vy + par.mass * gy + par.charge * ey - par.charge * par.vx * bz;
+	public double getForceY(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return - drag * par.vy + par.mass * gy + par.charge * (ey  + s.currentGrid.Ey[xCellPosition][yCellPosition]) -
+				par.charge * par.vx * (bz + s.currentGrid.Bz[xCellPosition][yCellPosition]);
 	}
 	
-	public double getPositionComponentofForceX(Particle2D par)
-	{
-		return par.mass * gx + par.charge * ex;
+	public double getPositionComponentofForceX(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return par.mass * gx + par.charge * (ex + s.currentGrid.Ex[xCellPosition][yCellPosition]);
 	}
 	
-	public double getPositionComponentofForceY(Particle2D par)
-	{
-		return par.mass * gy + par.charge * ey;
+	public double getPositionComponentofForceY(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return par.mass * gy + par.charge * (ey  + s.currentGrid.Ey[xCellPosition][yCellPosition]);
 	}
 	
-	public double getTangentVelocityComponentOfForceX(Particle2D par)
-	{
+	public double getTangentVelocityComponentOfForceX(Particle2D par) {
+		
 		return - drag * par.vx;
 	}
 	
-	public double getTangentVelocityComponentOfForceY(Particle2D par)
-	{
+	public double getTangentVelocityComponentOfForceY(Particle2D par) {
+		
 		return - drag * par.vy;
 	}
 	
-	public double getNormalVelocityComponentofForceX(Particle2D par)
-	{
-		return par.charge * par.vy * bz;
+	public double getNormalVelocityComponentofForceX(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return par.charge * par.vy * (bz + s.currentGrid.Bz[xCellPosition][yCellPosition]);
 	}
 	
-	public double getNormalVelocityComponentofForceY(Particle2D par)
-	{
-		return - par.charge * par.vx * bz;
+	public double getNormalVelocityComponentofForceY(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return - par.charge * par.vx * (bz + s.currentGrid.Bz[xCellPosition][yCellPosition]);
 	}
+	
+	public double getB(Particle2D par) {
+		
+		int xCellPosition = s.currentGrid.checkCellX(par);
+		int yCellPosition = s.currentGrid.checkCellY(par);
+		return bz + s.currentGrid.Bz[xCellPosition][yCellPosition];
+	}
+	
 }
