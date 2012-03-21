7ded30e5be437f1abfa9a23d1c83712a2a8d2b91
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-03-21 19:51:21 +0100

    Changing the methods equals and hashCode

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/util/Pair.java
@@ -10,14 +10,46 @@ public class Pair<A, B> {
 		this.second = second;
 	}
 
-	@Override public int hashCode() {
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((first == null) ? 0 : first.hashCode());
+		result = prime * result + ((second == null) ? 0 : second.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		if (first == null) {
+			if (other.first != null)
+				return false;
+		} else if (!first.equals(other.first))
+			return false;
+		if (second == null) {
+			if (other.second != null)
+				return false;
+		} else if (!second.equals(other.second))
+			return false;
+		return true;
+	}
+	
+	/*
+	 * @Override public int hashCode() {
 		int hashFirst = first != null ? first.hashCode() : 0;
 		int hashSecond = second != null ? second.hashCode() : 0;
 		
 		int hash = 0;
 		
 		hash = 37 * hash + hashFirst;
-		hash = 37 * hash + hashSecond;
+		hash = 57 * hash + hashSecond;
 		return hash;
 		//return (hashFirst + hashSecond) * hashFirst + hashSecond;
 	}
@@ -30,7 +62,7 @@ public class Pair<A, B> {
 					this.first.equals(anotherPair.first))) &&
 					(this.second == anotherPair.second || (this.second != null && anotherPair.second != null &&
 					this.second.equals(anotherPair.second))));
-					*/
+					
 			return
 					this.first == anotherPair.first && this.second == anotherPair.second;
 					//this.hashCode() == anotherPair.hashCode();
@@ -38,6 +70,7 @@ public class Pair<A, B> {
 		return false;
 		}
 	}
+	 */
 	
 	public int compareTo(Pair<A, B> another) {
 		return this.hashCode() < another.hashCode() ? -1 : 1;
