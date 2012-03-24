df6d1dc7164acf78421efce05ea1a116b31c388b
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-03-24 23:14:24 +0100

    added interpolateToParticle() call to getForce methods

--- pixi/src/main/java/org/openpixi/pixi/physics/force/SimpleGridForce.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/force/SimpleGridForce.java
@@ -16,53 +16,45 @@ public class SimpleGridForce extends Force {
 	//getting the force in the x - direction
 	@Override
 	public double getForceX(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return par.charge * s.currentGrid.Ex[xCellPosition][yCellPosition] +
-				par.charge * par.vy * s.currentGrid.Bz[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return par.charge * fields[0] + par.charge * par.vy * fields[2];
 	}
 
 	//getting the force in the y - direction
 	@Override
 	public double getForceY(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return par.charge * s.currentGrid.Ey[xCellPosition][yCellPosition] -
-				par.charge * par.vx * s.currentGrid.Bz[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return par.charge * fields[1] -
+				par.charge * par.vx * fields[2];
 	}
 
 	@Override
 	public double getPositionComponentofForceX(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return par.charge * s.currentGrid.Ex[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return par.charge * fields[0];
 	}
 
 	@Override
 	public double getPositionComponentofForceY(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return par.charge * s.currentGrid.Ey[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return par.charge * fields[1];
 	}
 
 	@Override
 	public double getNormalVelocityComponentofForceX(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return par.charge * par.vy * s.currentGrid.Bz[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return par.charge * par.vy * fields[2];
 	}
 
 	@Override
 	public double getNormalVelocityComponentofForceY(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return - par.charge * par.vx * s.currentGrid.Bz[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return - par.charge * par.vx * fields[2];
 	}
 
 	@Override
 	public double getBz(Particle2D par) {
-		int xCellPosition = s.currentGrid.checkCellX(par);
-		int yCellPosition = s.currentGrid.checkCellY(par);
-		return s.currentGrid.Bz[xCellPosition][yCellPosition];
+		double[] fields = s.currentGrid.interpolateToParticle(par);
+		return fields[2];
 	}
 }
