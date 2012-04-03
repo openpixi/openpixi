978226d4373b3cbe8f618787a00a033bcf1e61e1
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-04-03 11:45:39 +0200

    Deleting unnecessary comments

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllParticles.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllParticles.java
@@ -2,7 +2,6 @@ package org.openpixi.pixi.physics.collision.detectors;
 
 import java.util.ArrayList;
 import org.openpixi.pixi.physics.*;
-import org.openpixi.pixi.physics.collision.util.BoundingBox;
 import org.openpixi.pixi.physics.collision.util.Pair;
 
 public class AllParticles extends Detector{
@@ -28,11 +27,12 @@ public class AllParticles extends Detector{
 		for(int i = 0; i < (particlelist.size() - 1); i++)
 		{
 			Particle2D p1 = (Particle2D) particlelist.get(i);
-			//double x1 = Math.sqrt(p1.x * p1.x + p1.y * p1.y);
+			
 			for(int k = (i + 1); k < particlelist.size(); k++)
 			{
 				Particle2D p2 = (Particle2D) particlelist.get(k);
 				double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+				
 				if(distance <= (p1.radius + p2.radius)) {
 					
 					Pair<Particle2D, Particle2D> pair = new Pair<Particle2D, Particle2D>(p1, p2);
