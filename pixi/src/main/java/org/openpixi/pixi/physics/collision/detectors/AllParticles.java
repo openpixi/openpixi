40fc195e2285de1520ec31e8e41d648f2059d5a2
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-04-03 11:48:15 +0200

    Changing the algorithm

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllParticles.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/detectors/AllParticles.java
@@ -31,13 +31,15 @@ public class AllParticles extends Detector{
 			for(int k = (i + 1); k < particlelist.size(); k++)
 			{
 				Particle2D p2 = (Particle2D) particlelist.get(k);
-				double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+				//double distance = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
 				
-				if(distance <= (p1.radius + p2.radius)) {
+				if((p1.x - p2.x) <= (p1.radius + p2.radius)) {
 					
-					Pair<Particle2D, Particle2D> pair = new Pair<Particle2D, Particle2D>(p1, p2);
-					overlappedPairs.add(pair);
-					
+					if((p1.y - p2.y) <= (p1.radius + p2.radius)) {
+						
+						Pair<Particle2D, Particle2D> pair = new Pair<Particle2D, Particle2D>(p1, p2);
+						overlappedPairs.add(pair);
+					}					
 				}
 			}
 			
