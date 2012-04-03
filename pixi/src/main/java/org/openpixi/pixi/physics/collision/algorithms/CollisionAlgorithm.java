f77d900955c7e33e683af543deb1f9d6e8f3cf2b
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-04-03 11:33:13 +0200

    Deleting unnecessary code (a library that was not used, but imported)

--- pixi/src/main/java/org/openpixi/pixi/physics/collision/algorithms/CollisionAlgorithm.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/collision/algorithms/CollisionAlgorithm.java
@@ -3,7 +3,6 @@ package org.openpixi.pixi.physics.collision.algorithms;
 import java.util.ArrayList;
 
 import org.openpixi.pixi.physics.*;
-import org.openpixi.pixi.physics.collision.detectors.*;
 import org.openpixi.pixi.physics.collision.util.Pair;
 import org.openpixi.pixi.physics.force.Force;
 import org.openpixi.pixi.physics.solver.Solver;
