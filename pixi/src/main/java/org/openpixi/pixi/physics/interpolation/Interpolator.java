dc870898cf0f1e18a513ed9097e81ee84c1956df
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-05 23:47:49 +0200

    created new class and package for interpolation algorithms

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/interpolation/Interpolator.java
@@ -0,0 +1,23 @@
+package org.openpixi.pixi.physics.interpolation;
+
+import java.util.ArrayList;
+
+import org.openpixi.pixi.physics.Particle2D;
+import org.openpixi.pixi.physics.*;
+
+public class Interpolator {
+	
+	public Interpolator() {
+		
+	}
+	
+	public void interpolateToGrid(ArrayList<Particle2D> particles, CurrentGrid g) {
+		
+	}
+	
+	public void interpolateToParticle(ArrayList<Particle2D> particles) {
+		
+	}
+
+
+}
