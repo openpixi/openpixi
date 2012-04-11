2a0bf698651f77d60f256d591ca881fad86b2629
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-11 20:46:58 +0200

    added new type of grid and charge conserving interpolator (both not finished yet)

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/ChargeConservingAreaWeighting.java
@@ -0,0 +1,178 @@
+package org.openpixi.pixi.physics.grid;
+
+import java.util.ArrayList;
+
+import org.openpixi.pixi.physics.Particle2D;
+
+public class ChargeConservingAreaWeighting extends Interpolator {
+		
+	public ChargeConservingAreaWeighting(Grid g) {
+		
+		super(g);
+		g.particledata = new ArrayList<Particle2DData>(g.s.particles.size());
+		
+	}
+	
+	public void interpolateToGrid(ArrayList<Particle2D> particles) {
+		
+		for(int i = 0; i < g.numCellsX + 2; i++) {
+			for(int k = 0; k < g.numCellsY + 2; k++) {
+				g.jx[i][k] = 0.0;
+				g.jy[i][k] = 0.0;
+			}
+		}
+
+		//assuming rectangular particle shape i.e. area weighting
+		for (int i = 0; i < particles.size(); i++) {
+			
+			Particle2D p = particles.get(i);
+			Particle2DData pd = g.particledata.get(i);
+			
+			//boundary check
+			if (p.x < 0 || p.x > g.s.width || p.y < 0 || p.y > g.s.height) {
+				break;
+			}
+			
+			//local origin i.e. nearest grid point BEFORE particle push
+			int xStart = (int) (pd.x / g.cellWidth + 0.5 );
+			int yStart = (int) (pd.y / g.cellHeight+ 0.5);
+			
+			int xEnd = (int) (pd.x / g.cellWidth + 0.5 );
+			int yEnd = (int) (pd.y / g.cellHeight+ 0.5);
+			
+			double deltaX = p.x - pd.x;
+			double deltaY = p.y - pd.y;
+			
+			//4-boundary move?
+			if (xStart == xEnd && yStart == yEnd) {	
+				/**local x coordinate BEFORE particle push*/
+				double x = pd.x - xStart * g.cellWidth;
+				/**local y coordinate BEFORE particle push*/
+				double y = pd.y - yStart * g.cellHeight;
+				
+				fourBoundaryMove(xStart, yStart, x, y, deltaX, deltaY);				
+				
+				}
+			//7-boundary move?
+			else if (xStart == xEnd || yStart == yEnd) {
+				
+					sevenBoundaryMove(xStart, yStart, xEnd, yEnd, deltaX, deltaY, pd);
+					
+				}
+				// 10-boundary move
+					else {
+						
+						tenBoundaryMove(xStart, yStart, xEnd, yEnd, deltaX, deltaY, pd);
+						
+					}
+						
+			pd.x = p.x;
+			pd.y = p.y;
+			
+		}
+	}
+	
+	/**
+	 * Writes current to jx and jy arrays
+	 * @param lx x index of local origin
+	 * @param ly y index of local origin
+	 * @param x local x coordinate relative to lx BEFORE particle push
+	 * @param y local y coordinate relative to ly BEFORE particle push
+	 * @param deltaX x distance covered by particle (not absolute but only for this 4-boundary move)
+	 * @param deltaY y distance covered by particle (not absolute but only for this 4-boundary move)
+	 * @param p Particle2D
+	 */
+	private void fourBoundaryMove(int lx, int ly, double x, double y, double deltaX, double deltaY) {
+		//will lead to boundary problems!
+		g.jx[lx][ly-1] += deltaX * ((g.cellHeight - deltaY) / 2 - y);
+		g.jx[lx][ly] += deltaX * ((g.cellHeight + deltaY) / 2 + y);
+		g.jy[lx-1][ly] += deltaY * ((g.cellWidth - deltaX) / 2 - x);
+		g.jy[lx][ly] += deltaY * ((g.cellWidth + deltaX) / 2 + x);
+	}
+	
+	private void sevenBoundaryMove(int xStart, int yStart, int xEnd, int yEnd, double deltaX, double deltaY, Particle2DData pd) {
+		
+		/**local x coordinate BEFORE particle push*/
+		double x = pd.x - xStart * g.cellWidth;
+		/**local y coordinate BEFORE particle push*/
+		double y = pd.y - yStart * g.cellHeight;
+		
+		//7-boundary move with equal x?
+		if (xStart == xEnd) {
+			//particle moves to the right?
+			if (yEnd > yStart) {
+			
+				double deltaX1 = (g.cellWidth / 2) - x;
+				double deltaY1 = (deltaY / deltaX) * deltaX1;
+				fourBoundaryMove(xStart, yStart, x, y, deltaX1, deltaY1);
+				
+				deltaX -= deltaX1;
+				deltaY -= deltaY1;
+				y += deltaY1;
+				fourBoundaryMove(xEnd, yEnd, -(g.cellWidth / 2), y, deltaX, deltaY);
+							
+			}
+			//particle moves to the left
+			else {
+				
+				double deltaX1 = -((g.cellWidth / 2) + x);
+				double deltaY1 = (deltaY / deltaX) * deltaX1;
+				fourBoundaryMove(xStart, yStart, x, y, deltaX1, deltaY1);
+				
+				deltaX -= deltaX1;
+				deltaY -= deltaY1;
+				y += deltaY1;
+				fourBoundaryMove(xEnd, yEnd, (g.cellWidth / 2), y, deltaX, deltaY);
+				
+			}
+		}
+		//7-boundary move with equal y?
+		if (yStart == yEnd) {
+			//particle moves up?
+			if (xEnd > xStart) {
+				
+				double deltaY1 = (g.cellHeight / 2) - y;
+				double deltaX1 = (deltaX / deltaY) * deltaY1;
+				fourBoundaryMove(xStart, yStart, x, y, deltaX1, deltaY1);
+				
+				deltaX -= deltaX1;
+				deltaY -= deltaY1;
+				y += deltaY1;
+				fourBoundaryMove(xEnd, yEnd, x, -(g.cellHeight / 2), deltaX, deltaY);
+				
+			}
+			//particle moves down
+			else {
+				
+				double deltaY1 = -((g.cellHeight / 2) + y);
+				double deltaX1 = (deltaX / deltaY) * deltaY1;
+				fourBoundaryMove(xStart, yStart, x, y, deltaX1, deltaY1);
+				
+				deltaX -= deltaX1;
+				deltaY -= deltaY1;
+				y += deltaY1;
+				fourBoundaryMove(xEnd, yEnd, x, (g.cellWidth / 2), deltaX, deltaY);
+				
+			}
+		}
+
+	}
+	
+	private void tenBoundaryMove (int xStart, int yStart, int xEnd, int yEnd, double deltaX, double deltaY, Particle2DData pd) {
+		
+		/**local x coordinate BEFORE particle push*/
+		double x = pd.x - xStart * g.cellWidth;
+		/**local y coordinate BEFORE particle push*/
+		double y = pd.y - yStart * g.cellHeight;
+		
+		
+		
+	}
+
+	
+	public void interpolateToParticle(ArrayList<Particle2D> particles) {
+		
+	}
+
+
+}
---
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/YeeGrid.java
@@ -0,0 +1,33 @@
+package org.openpixi.pixi.physics.grid;
+
+import java.util.ArrayList;
+import org.openpixi.pixi.physics.*;
+
+public class YeeGrid extends Grid {
+	
+	public YeeGrid(Simulation s) {
+		
+		super(s);
+		interp = new ChargeConservingAreaWeighting(this);
+		
+		numCellsX = 10;
+		numCellsY = 10;
+		cellWidth = s.width/numCellsX;
+		cellHeight = s.height/numCellsY;
+		
+		jx = new double[numCellsX+2][numCellsY+2];
+		jy = new double[numCellsX+2][numCellsY+2];
+		rho = new double[numCellsX+2][numCellsY+2];
+		Ex = new double[numCellsX+2][numCellsY+2];
+		Ey = new double[numCellsX+2][numCellsY+2];
+		Bz = new double[numCellsX+2][numCellsY+2];
+		
+	}
+	
+	public void updateGrid(ArrayList<Particle2D> particles) {
+		
+		interp.interpolateToGrid(particles);
+		s.fsolver.step(this);
+	}
+
+}
