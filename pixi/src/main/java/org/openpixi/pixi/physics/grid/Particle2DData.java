884d7dec87eae121f1ff67e530d426509b79a831
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-11 20:45:21 +0200

    added position variables

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/Particle2DData.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/Particle2DData.java
@@ -1,9 +1,14 @@
 package org.openpixi.pixi.physics.grid;
 
 public class Particle2DData {
-		
+	
+	/**previous x position of particle*/
+	public double x;
+	/**previous y position of particle*/
+	public double y;
+	
 	public Particle2DData() {
 		
 	}
-
+	
 }
