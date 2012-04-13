e756a73e9eff8aaaf51cb05c0b2cb036be065ae2
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-13 16:25:47 +0200

    added Field variables to Particle2DData

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/Particle2DData.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/Particle2DData.java
@@ -7,6 +7,10 @@ public class Particle2DData {
 	/**previous y position of particle*/
 	public double y;
 	
+	public double Ex;
+	public double Ey;
+	public double Bz;
+	
 	public Particle2DData() {
 		
 	}
