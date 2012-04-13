864fc5db9fb56b247ca67e8069428733fc48bbbf
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-13 16:26:33 +0200

    added basis for interpolation using Particle2DData's field variables

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/ChargeConservingAreaWeighting.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/ChargeConservingAreaWeighting.java
@@ -429,7 +429,64 @@ public class ChargeConservingAreaWeighting extends Interpolator {
 	}
 
 	
-	public void interpolateToParticle(ArrayList<Particle2D> particles) {
+	public void interpolateToParticle(ArrayList<Particle2D> particles, ArrayList<Particle2DData> pd) {
+		
+		for (int i = 0; i < particles.size(); i++) {
+			
+		Particle2D p = g.s.particles.get(i);
+		int xCellPosition = (int) Math.floor(p.x / g.cellWidth + 1);
+		int yCellPosition = (int) Math.floor(p.y / g.cellHeight + 1);
+		
+		int xCellPosition2 = xCellPosition;
+		int yCellPosition2 = yCellPosition;
+		
+		if(xCellPosition >= g.numCellsX + 1) {
+			xCellPosition = g.numCellsX + 1;
+		} else if(xCellPosition < 1) {
+				xCellPosition = 1;
+		}
+		if(yCellPosition >= g.numCellsY + 1) {
+			yCellPosition = g.numCellsY + 1;
+		} else if(yCellPosition < 1) {
+			yCellPosition = 1;
+		}
+
+		if (Debug.asserts) {
+			// Assert conditions for interpolation
+			assert xCellPosition2 * g.cellWidth > p.x : p.x;
+			assert p.x > (xCellPosition2 - 1) * g.cellWidth : p.x;
+			assert yCellPosition2 * g.cellHeight > p.y : p.y;
+			assert p.y > (yCellPosition2 - 1) * g.cellHeight : p.y;
+		}
+
+		pd.get(i).Ex = ( g.Ex[xCellPosition][yCellPosition] * (xCellPosition2 * g.cellWidth - p.x) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Ex[xCellPosition + 1][yCellPosition] * (p.x - (xCellPosition2 - 1) * g.cellWidth) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Ex[xCellPosition][yCellPosition + 1] * (xCellPosition2 * g.cellWidth - p.x) *
+				(p.y - (yCellPosition2 - 1) * g.cellHeight) +
+				g.Ex[xCellPosition + 1][yCellPosition + 1] * (p.x - (xCellPosition2 - 1) * g.cellWidth) *
+				(p.y - (yCellPosition2 - 1) * g.cellHeight) ) / (g.cellWidth * g.cellHeight);
+		
+		pd.get(i).Ey = ( g.Ey[xCellPosition][yCellPosition] * (xCellPosition2 * g.cellWidth - p.x) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Ey[xCellPosition + 1][yCellPosition] * (p.x - (xCellPosition2 - 1) * g.cellWidth) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Ey[xCellPosition][yCellPosition + 1] * (xCellPosition2 * g.cellWidth - p.x) *
+				(p.y - (yCellPosition2 - 1) * g.cellHeight) +
+				g.Ey[xCellPosition + 1][yCellPosition + 1] * (p.x - (xCellPosition2 - 1) * g.cellWidth) *
+				(p.y - (yCellPosition2 - 1) * g.cellHeight) ) / (g.cellWidth * g.cellHeight);
+		
+		pd.get(i).Bz = ( g.Bz[xCellPosition][yCellPosition] * (xCellPosition2 * g.cellWidth - p.x) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Bz[xCellPosition + 1][yCellPosition] * (p.x - (xCellPosition2 - 1) * g.cellWidth) *
+				(yCellPosition2 * g.cellHeight - p.y) +
+				g.Bz[xCellPosition][yCellPosition + 1] * (xCellPosition2 * g.cellWidth - p.x) *
+				(p.y - (yCellPosition2 - 1) * g.cellHeight) +
+				g.Bz[xCellPosition + 1][yCellPosition + 1] * (p.x - (xCellPosition2 -1) * g.cellWidth) *
+				(p.y - (yCellPosition2 -1) * g.cellHeight) ) / (g.cellWidth * g.cellHeight);
+		
+		}
 		
 	}
 
