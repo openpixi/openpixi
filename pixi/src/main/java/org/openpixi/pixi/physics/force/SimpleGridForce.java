328c1272fd5ffd6f4ccf915986b1c36bfe4836c4
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-21 00:15:13 +0200

    removed interpolateToParticle() call

--- pixi/src/main/java/org/openpixi/pixi/physics/force/SimpleGridForce.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/force/SimpleGridForce.java
@@ -16,45 +16,38 @@ public class SimpleGridForce extends Force {
 	//getting the force in the x - direction
 	@Override
 	public double getForceX(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return par.charge * fields[0] + par.charge * par.vy * fields[2];
 	}
 
 	//getting the force in the y - direction
 	@Override
 	public double getForceY(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return par.charge * fields[1] -
 				par.charge * par.vx * fields[2];
 	}
 
 	@Override
 	public double getPositionComponentofForceX(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return par.charge * fields[0];
 	}
 
 	@Override
 	public double getPositionComponentofForceY(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return par.charge * fields[1];
 	}
 
 	@Override
 	public double getNormalVelocityComponentofForceX(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return par.charge * par.vy * fields[2];
 	}
 
 	@Override
 	public double getNormalVelocityComponentofForceY(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return - par.charge * par.vx * fields[2];
 	}
 
 	@Override
 	public double getBz(Particle2D par) {
-		double[] fields = s.currentGrid.interpolateToParticle(par);
 		return fields[2];
 	}
 }
