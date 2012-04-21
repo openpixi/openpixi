245f5cc5b36b228c484f5308976cb5fa54bc8a23
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-04-21 16:18:00 +0200

    Changing the effects

--- pixi/src/main/java/org/openpixi/pixi/physics/solver/relativistic/BorisRelativistic.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/relativistic/BorisRelativistic.java
@@ -21,11 +21,12 @@ package org.openpixi.pixi.physics.solver.relativistic;
 
 import org.openpixi.pixi.physics.*;
 import org.openpixi.pixi.physics.force.Force;
+import org.openpixi.pixi.physics.solver.Solver;
 
 /**The calculation is due to Boris and the equations((7) - (10)) can be found here:
  * http://ptsg.eecs.berkeley.edu/publications/Verboncoeur2005IOP.pdf
  */
-public class BorisRelativistic extends SolverRelativistic{
+public class BorisRelativistic extends Solver{
 	
 	public BorisRelativistic()
 	{
