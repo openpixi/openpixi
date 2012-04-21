130705d5adf82ce611a842f8e504339c2cb36b33
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-04-21 17:24:33 +0200

    added usual grid methods

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/YeeGrid.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/YeeGrid.java
@@ -16,19 +16,72 @@ public class YeeGrid extends Grid {
 
 		interp = new ChargeConservingAreaWeighting(this);
 
-		jx = new double[numCellsX+2][numCellsY+2];
-		jy = new double[numCellsX+2][numCellsY+2];
-		rho = new double[numCellsX+2][numCellsY+2];
-		Ex = new double[numCellsX+2][numCellsY+2];
-		Ey = new double[numCellsX+2][numCellsY+2];
-		Bz = new double[numCellsX+2][numCellsY+2];
+		jx = new double[numCellsX][numCellsY];
+		jy = new double[numCellsX][numCellsY];
+		rho = new double[numCellsX][numCellsY];
+		Ex = new double[numCellsX][numCellsY];
+		Ey = new double[numCellsX][numCellsY];
+		Bz = new double[numCellsX][numCellsY];
+		initFields();
 		
+	}
+	
+	//a method to change the dimensions of the cells, i.e. the width and the height
+	public void changeDimension(double width, double height, int xbox, int ybox)
+	{
+		numCellsX = xbox;
+		numCellsY = ybox;
+		
+		jx = new double[numCellsX][numCellsY];
+		jy = new double[numCellsX][numCellsY];
+		rho = new double[numCellsX][numCellsY];
+		Ex = new double[numCellsX][numCellsY];
+		Ey = new double[numCellsX][numCellsY];
+		Bz = new double[numCellsX][numCellsY];
+		initFields();
+		
+		setGrid(width, height);
+	}
+	
+	public void setGrid(double width, double height)
+	{
+		cellWidth = width / numCellsX;
+		cellHeight = height / numCellsY;
+		
+		for (Particle2D p: s.particles){
+			//assuming rectangular particle shape i.e. area weighting
+			p.pd.cd = p.charge / (cellWidth * cellHeight);
+		}
+		
+		//include updateGrid() and the first calculation of Fields here
 	}
 	
 	public void updateGrid(ArrayList<Particle2D> particles) {
 		
+		reset();
 		interp.interpolateToGrid(particles);
 		s.fsolver.step(this);
+		interp.interpolateToParticle(particles);
+	}
+	
+	private void reset() {
+		for(int i = 0; i < numCellsX; i++) {
+			for(int k = 0; k < numCellsY; k++) {
+				jx[i][k] = 0.0;
+				jy[i][k] = 0.0;
+				rho[i][k] = 0.0;
+			}
+		}
+	}
+	
+	private void initFields() {
+		for (int i = 0; i < numCellsX; i++) {
+			for (int j = 0; j < numCellsY; j++) {
+				Ex[i][j] = 0.0;
+				Ey[i][j] = 0.0;
+				Bz[i][j] = 0.0;
+			}
+		}
 	}
 
 }
