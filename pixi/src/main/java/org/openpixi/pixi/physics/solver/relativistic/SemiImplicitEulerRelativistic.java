31e2f0b2bb4586d19e39912f38bd49ffc8d0f228
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-04-23 18:16:54 +0200

    Making a relativistic version of the semi implicit Euler algorithm

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/relativistic/SemiImplicitEulerRelativistic.java
@@ -0,0 +1,57 @@
+/*
+ * OpenPixi - Open Particle-In-Cell (PIC) Simulator
+ * Copyright (C) 2012  OpenPixi.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openpixi.pixi.physics.solver.relativistic;
+
+import org.openpixi.pixi.physics.*;
+import org.openpixi.pixi.physics.force.Force;
+import org.openpixi.pixi.physics.solver.Solver;
+
+/**This class represents the simple Semi Implicit Euler algorithm.
+ * For more information: 
+ * http://en.wikipedia.org/wiki/Semi-implicit_Euler_method
+ */
+public class SemiImplicitEulerRelativistic extends Solver {
+	
+	public SemiImplicitEulerRelativistic()
+	{
+		super();
+	}
+	
+	/**
+	 * Semi Implicit Euler algorithm.
+	 * @param p before the update: x(t), v(t), a(t);
+	 *                 after the update: x(t+dt), v(t+dt), a(t);
+	 */
+	public void step(Particle2D p, Force f, double step)
+	{
+		//a(t) = F(v(t), x(t)) / m
+		p.ax = f.getForceX(p) / p.mass;
+		p.ay = f.getForceY(p) / p.mass;
+		
+		// v(t+dt) = v(t) + a(t)*dt
+		p.vx += p.ax * step / Math.sqrt(1 / (1 - (p.vx / ConstantsSI.c) * (p.vx / ConstantsSI.c)));
+		p.vy += p.ay * step / Math.sqrt(1 / (1 - (p.vx / ConstantsSI.c) * (p.vx / ConstantsSI.c)));
+		
+		// x(t+dt) = x(t) + v(t+dt)*dt
+		p.x += p.vx * step;
+		p.y += p.vy * step;
+		
+	}
+	
+}
