6c39824c25f39dd4757e231c81e190d4af206cc0
unknown <Kirill Streltsov@workstation.(none)>
2012-05-08 09:17:58 +0200

    extended the PoissonSolver

--- pixi/src/main/java/org/openpixi/pixi/physics/fields/PoissonSolver.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/PoissonSolver.java
@@ -1,6 +1,7 @@
 package org.openpixi.pixi.physics.fields;
 
 import edu.emory.mathcs.jtransforms.fft.*;
+import org.openpixi.pixi.physics.grid.Grid;
 import java.math.*;
 
 public class PoissonSolver {
@@ -9,21 +10,21 @@ public class PoissonSolver {
 		
 	}
 	
-	public static void solve2D(double[][] rho) {
+	public static void solve2D(Grid g) {
 		
-		int rows = rho.length;
-		int columns = 2 * rho[0].length;
+		int rows = g.rho.length;
+		int columns = 2 * g.rho[0].length;
 		int n = 0;
 		double[][] trho = new double[rows][columns];
 		double[][] phi = new double[rows][columns];
 
-		DoubleFFT_2D fft = new DoubleFFT_2D(rows, columns);
+		DoubleFFT_2D fft = new DoubleFFT_2D(rows, columns/2);
 		
 		//prepare input for fft
 		for(int i = 0; i < rows; i++) {
 			n = 0;
 			for(int j = 0; j < columns; j += 2) {
-				trho[i][j] = rho[i][j-n];
+				trho[i][j] = g.rho[i][j-n];
 				trho[i][j+1] = 0;
 				n += 1;
 			}
@@ -35,14 +36,36 @@ public class PoissonSolver {
 		//solve Poisson equation in Fourier space
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < columns; j += 2) {
-				phi[i][j] = (cellWidth * cellHeigth * trho[i][j]) /
-						(4 - Math.cos(2 * Math.PI * i * numCellsX) - Math.cos(2 * Math.PI * j * numCellsY))
+				double d = (4 - 2 * Math.cos(2 * Math.PI * i / g.numCellsX) - 2 * Math.cos(2 * Math.PI * j / g.numCellsY));
+				if (d != 0) {
+				phi[i][j] = (g.cellWidth * g.cellHeight * trho[i][j]) / d;						;
+//				phi[i][j+1] = (g.cellWidth * g.cellHeight * trho[i][j+1]) / d;
+				} else {
+					phi[i][j] = 0;
+					phi[i][j+1] = 0;
+				}
+//				System.out.println(trho[i][j] + " " + trho[i][j+1]);
 			}
 		}
 		
 		//perform inverse Fourier transform
 		fft.complexInverse(phi, true);
 		
+		for(int i = 0; i < rows; i++) {
+			n = 0;
+			for(int j = 0; j < columns; j += 2) {
+				System.out.println(phi[i][j] + " " + phi[i][j+1]);
+			}
+		}
+		
+		//prepare output
+		for(int i = 0; i < rows; i++) {
+			n = 0;
+			for(int j = 0; j < columns; j += 2) {
+				g.phi[i][j-n] = phi[i][j];
+				n += 1;
+			}
+		}
 		
 	}
 	
