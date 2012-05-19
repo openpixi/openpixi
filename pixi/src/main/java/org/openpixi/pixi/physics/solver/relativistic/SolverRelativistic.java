954c76b150c67053e515705a68d72d20e0a87f1d
Ognen Kapetanoski <kapetan.ok@gmail.com>
2012-05-19 16:45:08 +0200

    Basis class for the relativistic solvers

---
+++ pixi/src/main/java/org/openpixi/pixi/physics/solver/relativistic/SolverRelativistic.java
@@ -0,0 +1,19 @@
+package org.openpixi.pixi.physics.solver.relativistic;
+
+import org.openpixi.pixi.physics.ConstantsSI;
+
+public class SolverRelativistic {
+	
+	protected double c;
+	
+	public SolverRelativistic(double c) {
+		this.c = c;
+	}
+	
+	/**gamma = sqrt[1 + (u / c)^2]*/
+	public double calculateGamma(double ux, double uy) {		
+		double v = Math.sqrt(ux * ux + uy * uy);
+		return Math.sqrt(1 + (v / ConstantsSI.c) * (v / ConstantsSI.c));
+	}
+
+}
