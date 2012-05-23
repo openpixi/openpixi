50ee9087d2885c1dae27bc0991be966a4605f38c
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-05-23 21:51:13 +0200

    updated PoissonSolver for accessors use

--- pixi/src/main/java/org/openpixi/pixi/physics/fields/PoissonSolverPeriodic.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/PoissonSolverPeriodic.java
@@ -15,14 +15,14 @@ public class PoissonSolverPeriodic {
 	 * in the field variables of the Grid class. Note that periodic
 	 * boundaries are assumed by the transformation itself AND by the
 	 * derivative of the potential!</p>
-	 * @param g Grid on which the calculation shoudl be performed
+	 * @param g Grid on which the calculation should be performed
 	 */
 	public void solve(Grid g) {
 		
 		//size of the array to be transformed
-		int columns = g.rho.length;
-		int rows = g.rho[0].length;
-		double cellArea = g.cellWidth * g.cellHeight;
+		int rows = g.getNumCellsX();
+		int columns = g.getNumCellsY();
+		double cellArea = g.getCellWidth() * g.getCellHeight();
 		//JTransform saves the imaginary part as a second row entry
 		//therefore there must be twice as many rows
 		double[][] trho = new double[columns][2*rows];
@@ -33,7 +33,7 @@ public class PoissonSolverPeriodic {
 		//prepare input for fft
 		for(int i = 0; i < columns; i++) {
 			for(int j = 0; j < rows; j++) {
-				trho[i][2*j] = g.rho[i][j];
+				trho[i][2*j] = g.getRho(i,j);
 				trho[i][2*j+1] = 0;
 			}
 		}
@@ -46,14 +46,14 @@ public class PoissonSolverPeriodic {
 		//to the potential and can therefore be chosen arbitrarily.
 		for(int i = 1; i < columns; i++) {
 			for(int j = 0; j < rows; j++) {
-				double d = (4 - 2 * Math.cos((2 * Math.PI * i) / g.numCellsX) - 2 * Math.cos((2 * Math.PI * j) / g.numCellsY));
+				double d = (4 - 2 * Math.cos((2 * Math.PI * i) / g.getNumCellsX()) - 2 * Math.cos((2 * Math.PI * j) / g.getNumCellsY()));
 				phi[i][2*j] = (cellArea * trho[i][2*j]) / d;
 				phi[i][2*j+1] = (cellArea * trho[i][2*j+1]) / d;
 			}
 		}
 		//i=0 but j!=0
 		for(int j = 1; j < rows; j++) {
-			double d = (2 - 2 * Math.cos((2 * Math.PI * j) / g.numCellsY));
+			double d = (2 - 2 * Math.cos((2 * Math.PI * j) / g.getNumCellsY()));
 			phi[0][2*j] = (cellArea * trho[0][2*j]) / d;
 			phi[0][2*j+1] = (cellArea * trho[0][2*j+1]) / d;		
 		}
@@ -68,48 +68,48 @@ public class PoissonSolverPeriodic {
 				//the electric field in x direction is equal to the negative derivative of the 
 				//potential in x direction, analogous for y direction
 				//using central difference, omitting imaginary part since it should be 0 anyway
-				g.Ex[i][j] = -(phi[i+1][2*j] - phi[i-1][2*j]) / (2 * g.cellWidth);
-				g.Ey[i][j] = -(phi[i][2*(j+1)] - phi[i][2*(j-1)]) / (2 * g.cellHeight);
+				g.setEx(i, j, -(phi[i+1][2*j] - phi[i-1][2*j]) / (2 * g.getCellWidth()));
+				g.setEy(i, j, -(phi[i][2*(j+1)] - phi[i][2*(j-1)]) / (2 * g.getCellHeight()));
 			}
 		}
 		
 		//lower and upper boundaries
 		for(int i = 1; i < columns-1; i++) {
-			g.Ex[i][0] = -(phi[i+1][0] - phi[i-1][0]) / (2 * g.cellWidth);
+			g.setEx(i, 0, -(phi[i+1][0] - phi[i-1][0]) / (2 * g.getCellWidth()));
 			//forward difference
-			g.Ey[i][0] = -(phi[i][2] - phi[i][2*(columns-2)]) / (2 * g.cellHeight);
-			g.Ex[i][rows-1] = -(phi[i+1][2*(rows-1)] - phi[i-1][2*(rows-1)]) / (2 * g.cellWidth);
+			g.setEy(i, 0, -(phi[i][2] - phi[i][2*(columns-2)]) / (2 * g.getCellHeight()));
+			g.setEx(i, rows-1, -(phi[i+1][2*(rows-1)] - phi[i-1][2*(rows-1)]) / (2 * g.getCellWidth()));
 			//backward difference
-			g.Ey[i][rows-1] = -(phi[i][0] - phi[i][2*(rows-2)]) / (2 * g.cellHeight);
+			g.setEy(i, rows-1, -(phi[i][0] - phi[i][2*(rows-2)]) / (2 * g.getCellHeight()));
 		}
 		
 		//left and right boundaries
 		for(int j = 1; j < rows-1; j++) {
 			//forward difference
-			g.Ex[0][j] = -(phi[1][2*j] - phi[rows-1][2*j]) / (2 * g.cellWidth);
-			g.Ey[0][j] = -(phi[0][2*(j+1)] - phi[0][2*(j-1)]) / (2 * g.cellHeight);
+			g.setEx(0, j, -(phi[1][2*j] - phi[rows-1][2*j]) / (2 * g.getCellWidth()));
+			g.setEy(0, j, -(phi[0][2*(j+1)] - phi[0][2*(j-1)]) / (2 * g.getCellHeight()));
 			//backward difference
-			g.Ex[columns-1][j] = -(phi[0][2*j] - phi[columns-2][2*j]) / (2 * g.cellWidth);
-			g.Ey[columns-1][j] = -(phi[columns-1][2*(j+1)] - phi[columns-1][2*(j-1)]) / (2 * g.cellHeight);
+			g.setEx(columns-1, j, -(phi[0][2*j] - phi[columns-2][2*j]) / (2 * g.getCellWidth()));
+			g.setEy(columns-1, j, -(phi[columns-1][2*(j+1)] - phi[columns-1][2*(j-1)]) / (2 * g.getCellHeight()));
 		}
 		
 		//4 boundary points
-		g.Ex[0][0] = -(phi[1][0] - phi[0][0]) / (2 * g.cellWidth);
-		g.Ey[0][0] = -(phi[0][2] - phi[0][0]) / (2 * g.cellHeight);
+		g.setEx(0, 0, -(phi[1][0] - phi[0][0]) / (2 * g.getCellWidth()));
+		g.setEy(0, 0, -(phi[0][2] - phi[0][0]) / (2 * g.getCellHeight()));
 		
-		g.Ex[0][rows-1] = -(phi[1][2*(rows-1)] - phi[0][2*(rows-1)]) / (2 * g.cellWidth);
-		g.Ey[0][rows-1] = -(phi[0][2*(rows-1)] - phi[0][2*(rows-2)]) / (2 * g.cellHeight);
+		g.setEx(0, rows-1, -(phi[1][2*(rows-1)] - phi[0][2*(rows-1)]) / (2 * g.getCellWidth()));
+		g.setEy(0, rows-1, -(phi[0][2*(rows-1)] - phi[0][2*(rows-2)]) / (2 * g.getCellHeight()));
 		
-		g.Ex[columns-1][rows-1] = -(phi[columns-1][2*(rows-1)] - phi[columns-2][2*(rows-1)]) / (2 * g.cellWidth);
-		g.Ey[columns-1][rows-1] = -(phi[columns-1][2*(rows-1)] - phi[columns-1][2*(rows-2)]) / (2 * g.cellHeight);		
+		g.setEx(columns-1, rows-1, -(phi[columns-1][2*(rows-1)] - phi[columns-2][2*(rows-1)]) / (2 * g.getCellWidth()));
+		g.setEy(columns-1, rows-1, -(phi[columns-1][2*(rows-1)] - phi[columns-1][2*(rows-2)]) / (2 * g.getCellHeight()));		
 		
-		g.Ex[columns-1][0] = -(phi[columns-1][0] - phi[columns-2][0]) / (2 * g.cellWidth);
-		g.Ey[columns-1][0] = -(phi[columns-1][2] - phi[columns-1][0]) / (2 * g.cellHeight);
+		g.setEx(columns-1, 0, -(phi[columns-1][0] - phi[columns-2][0]) / (2 * g.getCellWidth()));
+		g.setEy(columns-1, 0, -(phi[columns-1][2] - phi[columns-1][0]) / (2 * g.getCellHeight()));
 		
 		//prepare output
 		for(int i = 0; i < columns; i++) {
 			for(int j = 0; j < rows; j++) {
-				g.phi[i][j] = phi[i][2*j];
+				g.setPhi(i, j, phi[i][2*j]);
 			}
 		}
 		
