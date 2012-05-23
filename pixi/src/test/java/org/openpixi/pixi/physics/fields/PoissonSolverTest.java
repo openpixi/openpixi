58b6c24c787541d60123f3f11dd994d18156db47
kstrel <kirill.streltsov@student.tuwien.ac.at>
2012-05-23 22:47:31 +0200

    added internal classes for charge distributions

--- pixi/src/test/java/org/openpixi/pixi/physics/fields/PoissonSolverTest.java
+++ pixi/src/test/java/org/openpixi/pixi/physics/fields/PoissonSolverTest.java
@@ -13,20 +13,33 @@ import org.openpixi.pixi.ui.util.WriteFile;
 
 public class PoissonSolverTest extends TestCase {
 
+	double ACCURACY_LIMIT = 1.e-16;
+
 	public PoissonSolverTest(String testName){
 		super(testName);
+	}
+	
+	void assertAlmostEquals(String text, double x, double y, double limit) {
+		if ((Math.abs(x - y) / Math.abs(x + y) > limit)
+				|| (Double.isNaN(x) != Double.isNaN(y))
+				|| (Double.isInfinite(x) != Double.isInfinite(y))) {
+			assertTrue(text + " expected:<" + x + "> but was:<" + y + ">", false);
+		}
 	}
 
 	public void testPointcharge() {
 
 		Simulation s = InitialConditions.initEmptySimulation();
-		s.width = 10;
+		s.width = 75;
 		s.height = 10;
 		
-		Grid g = GridFactory.createYeeGrid(s, 100, 100, 10, 10);
+		Grid g = GridFactory.createYeeGrid(s, 95, 10, s.width, s.height);
 		g.resetCurrentAndCharge();
 		
 		
+		//writes to g.rho
+		ChargeDistribution charged = new PointChargeDistribution(g);
+
 //		g.rho = pointChargeShifted(g.numCellsX, g.numCellsY);
 //		g.rho = pointCharge(g.numCellsX, g.numCellsY);		
 //		g.rho = dipole(g.numCellsX, g.numCellsY);
@@ -41,48 +54,44 @@ public class PoissonSolverTest extends TestCase {
 		long elapsed = System.currentTimeMillis()-start;
 		System.out.println("\nCalculation time: "+elapsed);
 		
-		//deletes the old files
-		File file1 = new File("\\efeld.dat");
-		file1.delete();
-		File file2 = new File("\\potential.dat");
-		file2.delete();
+		int indexX = (int)(g.getNumCellsX()/2);
+		int indexY = (int) (g.getNumCellsY()/2);
+//		assertAlmostEquals("", (g.Ey[indexX][indexY+1]/16), g.Ey[indexX][indexY+4] , ACCURACY_LIMIT);
+////		charged.test();		
+		output(g);
+	}
+	
+	private double[][] pointCharge(int numCellsX, int numCellsY) {
+		double[][] rho = new double[numCellsX][numCellsY];
+		int indexX = (int)(numCellsX/2);
+		int indexY = (int) (numCellsY/2);
+		rho[indexX][indexY] = 50;
+		return rho;
+	}
+	
+	public interface ChargeDistribution {			
+		/**Tests PoisonSolver and the analytic results*/
+		void test();
+	}
+	
+	public class PointChargeDistribution implements ChargeDistribution {
 		
-		//creates new file "efield.dat" in working directory and writes
-		//field data to it
-		WriteFile fieldFile = new WriteFile("efeld", "");
-		for (int i = 0; i < g.getNumCellsX(); i++) {
-			for(int j = 0; j < g.getNumCellsY(); j++) {
-				fieldFile.writeLine(i*g.getCellWidth() + "\t" + j*g.getCellHeight() +
-						"\t" + g.getEx(i, j) + "\t" + g.getEy(i, j));
-			}
-		}
-		fieldFile.closeFstream();
+		Grid g;
+		int indexX;
+		int indexY;
 		
-		WriteFile potentialFile = new WriteFile("potential", "");
-		for (int i = 0; i < g.getNumCellsX(); i++) {
-			for(int j = 0; j < g.getNumCellsY(); j++) {
-				potentialFile.writeLine(i*g.getCellWidth() + "\t" + j*g.getCellHeight() + "\t" + g.getPhi(i, j));
-			}
-		}
-		potentialFile.closeFstream();
-		
-		//YOU NEED GNUPLOT FOR THIS http://www.gnuplot.info/
-		//NEEDS TO BE IN YOUR EXECUTION PATH (i.e. PATH variable on windows)
-		//plots the above output as vector field
-		try {
-		Runtime gnuplotrt = Runtime.getRuntime();
-		Process gnuplotpr = gnuplotrt.exec("gnuplot -e \"set term png; set output 'D:\\efield.png'; plot 'D:\\efeld.dat' using 1:2:3:4 with vectors head filled lt 2\"");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		//plots potential
-		try {
-		Runtime gnuplotPotentialRt = Runtime.getRuntime();
-		Process gnuplotPotentialPr = gnuplotPotentialRt.exec("gnuplot -e \"set term png; set output 'D:\\potential.png'; plot 'D:\\potential.dat' using 1:2:3 with circles linetype palette \"");
-		} catch (Exception e) {
-			e.printStackTrace();
+		PointChargeDistribution(Grid g) {
+			this.g = g;
+			int indexX = (int)(g.getNumCellsX()/2);
+			int indexY = (int) (g.getNumCellsY()/2);
+			g.setRho(indexX, indexY, 50);
 		}
 		
+		public void test() {
+			
+			assertAlmostEquals("", g.getEx(indexX, indexY)/(g.getCellWidth()*g.getCellWidth()), g.getEx(indexX+1, indexY) , ACCURACY_LIMIT);
+
+		}
 		
 	}
 	
@@ -98,14 +107,6 @@ public class PoissonSolverTest extends TestCase {
 			}
 		}
 		
-		return rho;
-	}
-	
-	private double[][] pointCharge(int numCellsX, int numCellsY) {
-		double[][] rho = new double[numCellsX][numCellsY];
-		int indexX = (int)(numCellsX/2);
-		int indexY = (int) (numCellsY/2);
-		rho[indexX][indexY] = 50;
 		return rho;
 	}
 	
@@ -166,5 +167,51 @@ public class PoissonSolverTest extends TestCase {
 
 		fft.complexForward(field);
 
+	}
+	
+	public void output(Grid g) {
+		
+		double aspectratio = g.simulation.height/g.simulation.width;
+		//deletes the old files
+		File file1 = new File("\\efeld.dat");
+		file1.delete();
+		File file2 = new File("\\potential.dat");
+		file2.delete();
+		
+		//creates new file "efield.dat" in working directory and writes
+		//field data to it
+		WriteFile fieldFile = new WriteFile("efeld", "");
+		for (int i = 0; i < g.getNumCellsX(); i++) {
+			for(int j = 0; j < g.getNumCellsY(); j++) {
+				fieldFile.writeLine(i*g.getCellWidth() + "\t" + j*g.getCellHeight() +
+						"\t" + g.getEx(i, j) + "\t" + g.getEy(i, j));
+			}
+		}
+		fieldFile.closeFstream();
+		
+		WriteFile potentialFile = new WriteFile("potential", "");
+		for (int i = 0; i < g.getNumCellsX(); i++) {
+			for(int j = 0; j < g.getNumCellsY(); j++) {
+				potentialFile.writeLine(i*g.getCellWidth() + "\t" + j*g.getCellHeight()  + "\t" + g.getPhi(i, j));
+			}
+		}
+		potentialFile.closeFstream();
+		
+		//YOU NEED GNUPLOT FOR THIS http://www.gnuplot.info/
+		//NEEDS TO BE IN YOUR EXECUTION PATH (i.e. PATH variable on windows)
+		//plots the above output as vector field
+		try {
+		Runtime gnuplotrt = Runtime.getRuntime();
+		Process gnuplotpr = gnuplotrt.exec("gnuplot -e \"set term png; set size ratio " + aspectratio + "; set output 'D:\\efield.png'; plot 'D:\\efeld.dat' using 1:2:3:4 with vectors head filled lt 2\"");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//plots potential
+		try {
+		Runtime gnuplotPotentialRt = Runtime.getRuntime();
+		Process gnuplotPotentialPr = gnuplotPotentialRt.exec("gnuplot -e \"set term png; set size ratio " + aspectratio + "; set output 'D:\\potential.png'; plot 'D:\\potential.dat' using 1:2:3 with circles linetype palette \"");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
