72db8ae141de108f82b3649b759761358b223f7f
Kirill Streltsov <kirill.streltsov@gmail.com>
2013-06-11 18:55:52 +0200

    Added method parameters

--- pixi/src/main/java/org/openpixi/pixi/physics/ParticleLoader.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/ParticleLoader.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface ParticleLoader {
 	
-	public List<Particle> load();
+	public List<Particle> load (int numOfParticles, int numCellsX, int numCellsY,
+			double simulationWidth, double simulationHeight, double radius);
 	
 }
