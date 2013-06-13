6e137418b65670874ece194166e7d99c01875a34
Kirill Streltsov <kirill.streltsov@gmail.com>
2013-06-13 16:24:28 +0200

    Current interpolation now takes into account that the current fields are located on shifted grids. The new implementation is the same as in the charge and "to particle" interpolation methods.

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/CloudInCell.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/CloudInCell.java
@@ -7,63 +7,80 @@ public class CloudInCell implements InterpolatorAlgorithm {
 
 	@Override
 	public void interpolateToGrid(Particle p, Grid g, double tstep) {
+		double cellArea = g.getCellWidth() * g.getCellHeight();
 
-		int xCellPosition = (int) (Math.floor((p.getX() / g.getCellWidth())));
-		int yCellPosition = (int) (Math.floor((p.getY() / g.getCellHeight())));
-
-		int xCellPosition2 = xCellPosition + 1;
-		int yCellPosition2 = yCellPosition + 1;
-
-		if (Debug.asserts) {
-			// Assert conditions for interpolation
-			assert xCellPosition2 * g.getCellWidth() > p.getX() : p.getX();
-			assert p.getX() > (xCellPosition2 - 1) * g.getCellWidth() : p.getX();
-			assert yCellPosition2 * g.getCellHeight() > p.getY() : p.getY();
-			assert p.getY() > (yCellPosition2 - 1) * g.getCellHeight() : p.getY();
+		//Determine the nearest lower left grid point
+		//THIS CAN BE NEGATIVE if particle is behind the left or the lower boundary
+		int i = (int) Math.floor(p.getX() / g.getCellWidth());
+		int j = (int) Math.floor(p.getY() / g.getCellHeight());
+		
+		/**Distance to the left cell boundary*/
+		double a;
+		/**Distance to the right cell boundary*/
+		double b;
+		/**Distance to the lower cell boundary*/
+		double c;
+		/**Distance to the upper cell boundary*/
+		double d;
+		
+		a = p.getX() - i * g.getCellWidth();
+		b = g.getCellWidth() - a;
+		c = p.getY() -  j * g.getCellHeight();
+		d = g.getCellHeight() - c;
+		
+		//The Jx-field is located in the middle of the left cell boundary.
+		//This means that the Jx-field-grid is shifted upwards by half a cell height.
+		//The adjustments are made to calculate the distance to the shifted grid. The
+		//only changes to be made are in the vertical plane. All changes are reversed
+		//after the calculation.
+		if( c < g.getCellHeight()/2 ){
+			j -= 1;
+			c += g.getCellHeight()/2;
+			
+			g.addJx(i, j, p.getCharge() * p.getVx() * b * d / cellArea);
+			g.addJx(i,j+1, p.getCharge() * p.getVx() * b * c / cellArea);
+			g.addJx(i + 1,j + 1, p.getCharge() * p.getVx() * a * c / cellArea);
+			g.addJx(i+1, j, p.getCharge() * p.getVx() * a * d / cellArea);
+			
+			c -= g.getCellHeight()/2;
+			j += 1;
+		} else {
+			c -= g.getCellHeight()/2;
+			
+			g.addJx(i, j, p.getCharge() * p.getVx() * b * d / cellArea);
+			g.addJx(i,j+1, p.getCharge() * p.getVx() * b * c / cellArea);
+			g.addJx(i + 1,j + 1, p.getCharge() * p.getVx() * a * c / cellArea);
+			g.addJx(i+1, j, p.getCharge() * p.getVx() * a * d / cellArea);
+			
+			c += g.getCellHeight()/2;			
 		}
 
-		g.addJx(xCellPosition, yCellPosition,
-				p.getCharge() * p.getVx() *
-				(xCellPosition2 * g.getCellWidth() - p.getX()) *
-				(yCellPosition2 * g.getCellHeight() - p.getY()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJx(xCellPosition + 1, yCellPosition,
-				p.getCharge() * p.getVx() *
-				(p.getX() - (xCellPosition2-1) * g.getCellWidth()) *
-				(yCellPosition2 * g.getCellHeight() - p.getY()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJx(xCellPosition,yCellPosition + 1,
-				p.getCharge() * p.getVx() *
-				(xCellPosition2 * g.getCellWidth() - p.getX()) *
-				(p.getY() - (yCellPosition2-1) * g.getCellHeight()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJx(xCellPosition + 1,yCellPosition + 1,
-				p.getCharge() * p.getVx() *
-				(p.getX() - (xCellPosition2-1) * g.getCellWidth()) *
-				(p.getY() - (yCellPosition2-1) * g.getCellHeight()) /
-				(g.getCellWidth() * g.getCellHeight()));
-
-		g.addJy(xCellPosition, yCellPosition,
-				p.getCharge() * p.getVy() *
-				(xCellPosition2 * g.getCellWidth() - p.getX()) *
-				(yCellPosition2 * g.getCellHeight() - p.getY()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJy(xCellPosition + 1, yCellPosition,
-				p.getCharge() * p.getVy() *
-				(p.getX() - (xCellPosition2-1) * g.getCellWidth()) *
-				(yCellPosition2 * g.getCellHeight() - p.getY()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJy(xCellPosition, yCellPosition + 1,
-				p.getCharge() * p.getVy() *
-				(xCellPosition2 * g.getCellWidth() - p.getX()) *
-				(p.getY() - (yCellPosition2-1) * g.getCellHeight()) /
-				(g.getCellWidth() * g.getCellHeight()));
-		g.addJy(xCellPosition + 1, yCellPosition + 1,
-				p.getCharge() * p.getVy() *
-				(p.getX() - (xCellPosition2-1) * g.getCellWidth()) *
-				(p.getY() - (yCellPosition2-1) * g.getCellHeight()) /
-				(g.getCellWidth() * g.getCellHeight()));
-
+		//The Jy-field is located in the middle of the lower cell boundary.
+		//This means that the Jy-field-grid is shifted to the right by half a cell width.
+		//The adjustments are made to calculate the distance to the shifted grid. The
+		//only changes to be made are in the horizontal plane. All changes are reversed
+		//after the calculation.
+		if( a < g.getCellWidth()/2 ){
+			i -= 1;
+			c += g.getCellWidth()/2;
+			
+			g.addJy(i, j, p.getCharge() * p.getVy() * b * d / cellArea);
+			g.addJy(i,j+1, p.getCharge() * p.getVy() * b * c / cellArea);
+			g.addJy(i + 1,j + 1, p.getCharge() * p.getVy() * a * c / cellArea);
+			g.addJy(i+1, j, p.getCharge() * p.getVy() * a * d / cellArea);
+			
+			c -= g.getCellWidth()/2;
+			i += 1;
+		} else {
+			c -= g.getCellWidth()/2;
+			
+			g.addJy(i, j, p.getCharge() * p.getVy() * b * d / cellArea);
+			g.addJy(i,j+1, p.getCharge() * p.getVy() * b * c / cellArea);
+			g.addJy(i + 1,j + 1, p.getCharge() * p.getVy() * a * c / cellArea);
+			g.addJy(i+1, j, p.getCharge() * p.getVy() * a * d / cellArea);
+			
+			c += g.getCellWidth()/2;			
+		}
 	}
 
 	@Override
@@ -134,7 +151,7 @@ public class CloudInCell implements InterpolatorAlgorithm {
 				a, b, c, d)) / 2);
 		
 		//The Ex-field is located in the middle of the left cell boundary.
-		//This means that the Ey-field-grid is shifted upwards by half a cell height.
+		//This means that the Ex-field-grid is shifted upwards by half a cell height.
 		//The adjustments are made to calculate the distance to the shifted grid. The
 		//only changes to be made are in the vertical plane. All changes are reversed
 		//after the calculation.
@@ -200,4 +217,5 @@ public class CloudInCell implements InterpolatorAlgorithm {
 			double a, double b, double c, double d){
 		return A*b*d + B*b*c + C*a*c + D*a*d;
 	}
+
 }
