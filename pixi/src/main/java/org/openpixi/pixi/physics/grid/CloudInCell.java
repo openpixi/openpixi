ee29cda1654bac253816db1e9dca1025b0006b24
Kirill Streltsov <kirill.streltsov@gmail.com>
2013-06-15 18:58:39 +0200

    Optimized interpolation to grid by using normalized coordinates.

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/CloudInCell.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/CloudInCell.java
@@ -1,89 +1,89 @@
 package org.openpixi.pixi.physics.grid;
 
-import org.openpixi.pixi.physics.Debug;
 import org.openpixi.pixi.physics.Particle;
 
 public class CloudInCell implements InterpolatorAlgorithm {
 
 	@Override
 	public void interpolateToGrid(Particle p, Grid g, double tstep) {
-		double cellArea = g.getCellWidth() * g.getCellHeight();
-
-		//Determine the nearest lower left grid point
-		//THIS CAN BE NEGATIVE if particle is behind the left or the lower boundary
-		int i = (int) Math.floor(p.getX() / g.getCellWidth());
-		int j = (int) Math.floor(p.getY() / g.getCellHeight());
-		
-		/**Distance to the left cell boundary*/
+		/**X index of the grid point that is left from or at the x position of the particle*/
+		int i;
+		/**Y index of the grid point that is below or at the y position of the particle*/
+		int j;		
+		/**Normalized distance to the left cell boundary*/
 		double a;
-		/**Distance to the right cell boundary*/
+		/**Normalized distance to the right cell boundary*/
 		double b;
-		/**Distance to the lower cell boundary*/
+		/**Normalized distance to the lower cell boundary*/
 		double c;
-		/**Distance to the upper cell boundary*/
+		/**Normalized distance to the upper cell boundary*/
 		double d;
 		
-		a = p.getX() - i * g.getCellWidth();
-		b = g.getCellWidth() - a;
-		c = p.getY() -  j * g.getCellHeight();
-		d = g.getCellHeight() - c;
+		a = p.getX() / g.getCellWidth();
+		i = (int) Math.floor(a);
+		a -= i;
+		b = 1 - a;
+		
+		c = p.getY() / g.getCellHeight();
+		j = (int) Math.floor(c);
+		c -= j;
+		d = 1 - c;
 		
 		//The Jx-field is located in the middle of the left cell boundary.
 		//This means that the Jx-field-grid is shifted upwards by half a cell height.
 		//The adjustments are made to calculate the distance to the shifted grid. The
 		//only changes to be made are in the vertical plane. All changes are reversed
 		//after the calculation.
-		if( c < g.getCellHeight()/2 ){
+		if( c < 0.5 ){
 			j -= 1;
-			c += g.getCellHeight()/2;
-			d = g.getCellHeight() - c;
+			c += 0.5;
+			d -= 0.5;
 			
-			g.addJx(i, j, p.getCharge() * p.getVx() * b * d / cellArea);
-			g.addJx(i,j+1, p.getCharge() * p.getVx() * b * c / cellArea);
-			g.addJx(i + 1,j + 1, p.getCharge() * p.getVx() * a * c / cellArea);
-			g.addJx(i+1, j, p.getCharge() * p.getVx() * a * d / cellArea);
+			g.addJx(i,		j,		p.getCharge() * p.getVx() * b * d);
+			g.addJx(i,		j + 1,	p.getCharge() * p.getVx() * b * c);
+			g.addJx(i + 1,	j + 1,	p.getCharge() * p.getVx() * a * c);
+			g.addJx(i + 1,	j,		p.getCharge() * p.getVx() * a * d);
 			
-			c -= g.getCellHeight()/2;
-			d = g.getCellHeight() - c;
+			c -= 0.5;
+			d += 0.5;
 			j += 1;
 		} else {
-			c -= g.getCellHeight()/2;
-			d = g.getCellHeight() - c;
+			c -= 0.5;
+			d += 0.5;
 			
-			g.addJx(i, j, p.getCharge() * p.getVx() * b * d / cellArea);
-			g.addJx(i,j+1, p.getCharge() * p.getVx() * b * c / cellArea);
-			g.addJx(i + 1,j + 1, p.getCharge() * p.getVx() * a * c / cellArea);
-			g.addJx(i+1, j, p.getCharge() * p.getVx() * a * d / cellArea);
+			g.addJx(i,		j,		p.getCharge() * p.getVx() * b * d);
+			g.addJx(i,		j + 1,	p.getCharge() * p.getVx() * b * c);
+			g.addJx(i + 1,	j + 1,	p.getCharge() * p.getVx() * a * c);
+			g.addJx(i + 1,	j,		p.getCharge() * p.getVx() * a * d);
 			
-			c += g.getCellHeight()/2;
-			d = g.getCellHeight() - c;
+			c += 0.5;
+			d -= 0.5;
 		}
 
 		//The Jy-field is located in the middle of the lower cell boundary.
 		//This means that the Jy-field-grid is shifted to the right by half a cell width.
 		//The adjustments are made to calculate the distance to the shifted grid. The
-		//only changes to be made are in the horizontal plane. All changes are reversed
-		//after the calculation.
-		if( a < g.getCellWidth()/2 ){
+		//only changes to be made are in the horizontal plane.
+		if( a < 0.5 ){
 			i -= 1;
-			a += g.getCellWidth()/2;
-			b = g.getCellWidth() - a;
+			a += 0.5;
+			b -= 0.5;
 			
-			g.addJy(i, j, p.getCharge() * p.getVy() * b * d / cellArea);
-			g.addJy(i,j+1, p.getCharge() * p.getVy() * b * c / cellArea);
-			g.addJy(i + 1,j + 1, p.getCharge() * p.getVy() * a * c / cellArea);
-			g.addJy(i+1, j, p.getCharge() * p.getVy() * a * d / cellArea);
+			g.addJy(i,		j,		p.getCharge() * p.getVy() * b * d);
+			g.addJy(i,		j + 1,	p.getCharge() * p.getVy() * b * c);
+			g.addJy(i + 1,	j + 1,	p.getCharge() * p.getVy() * a * c);
+			g.addJy(i + 1,	j,		p.getCharge() * p.getVy() * a * d);
 			
 			//No need to return the values to their previous state because they are
 			//not going to be used anymore.
 		} else {
-			a -= g.getCellWidth()/2;
-			b = g.getCellWidth() - a;
+			a -= 0.5;
+			b += 0.5;
 			
-			g.addJy(i, j, p.getCharge() * p.getVy() * b * d / cellArea);
-			g.addJy(i,j+1, p.getCharge() * p.getVy() * b * c / cellArea);
-			g.addJy(i + 1,j + 1, p.getCharge() * p.getVy() * a * c / cellArea);
-			g.addJy(i+1, j, p.getCharge() * p.getVy() * a * d / cellArea);
+			g.addJy(i,		j,		p.getCharge() * p.getVy() * b * d);
+			g.addJy(i,		j + 1,	p.getCharge() * p.getVy() * b * c);
+			g.addJy(i + 1,	j + 1,	p.getCharge() * p.getVy() * a * c);
+			g.addJy(i + 1,	j,		p.getCharge() * p.getVy() * a * d);
 			
 			//No need to return the values to their previous state because they are
 			//not going to be used anymore.			
@@ -92,37 +92,37 @@ public class CloudInCell implements InterpolatorAlgorithm {
 
 	@Override
 	public void interpolateChargedensity(Particle p, Grid g) {
-		double cellArea = g.getCellWidth() * g.getCellHeight();
-
-		//Determine the nearest lower left grid point on a grid that is shifted
-		//upward and to the right by half a cell height and width respectively.
-		//THIS CAN BE NEGATIVE if particle is behind the left or the lower boundary
-		int i = (int) (Math.floor( p.getX() / g.getCellWidth() - 0.5));
-		int j = (int) (Math.floor( p.getY() / g.getCellHeight() - 0.5));
-		
-		/**Distance to the left cell boundary*/
+		/**X index of the grid point that is left from or at the x position of the particle*/
+		int i;
+		/**Y index of the grid point that is below or at the y position of the particle*/
+		int j;		
+		/**Normalized distance to the left cell boundary*/
 		double a;
-		/**Distance to the right cell boundary*/
+		/**Normalized distance to the right cell boundary*/
 		double b;
-		/**Distance to the lower cell boundary*/
+		/**Normalized distance to the lower cell boundary*/
 		double c;
-		/**Distance to the upper cell boundary*/
+		/**Normalized distance to the upper cell boundary*/
 		double d;
 		
-		//The -0.5*cellWidth is there to shift the grid
-		a = p.getX() - (i + 0.5) * g.getCellWidth();
-		b = g.getCellWidth() - a;
+		//The -0.5 is there to shift the grid
+		a = p.getX() / g.getCellWidth() - 0.5;
+		i = (int) Math.floor(a);
+		a -= i;
+		b = 1 - a;
 
-		//The -0.5*cellWidth is there to shift the grid
-		c = p.getY() -  (j + 0.5) * g.getCellHeight();
-		d = g.getCellHeight() - c;
+		//The -0.5 is there to shift the grid
+		c = p.getY() / g.getCellHeight() - 0.5;
+		j = (int) Math.floor(c);
+		c -= j;
+		d = 1 - c;	
 
 		//Assign a portion of the charge to the four surrounding points depending on
 		//the distance.
-		g.addRho(i, j, p.getCharge() * b * d / cellArea);
-		g.addRho(i,j+1, p.getCharge() * b * c / cellArea);
-		g.addRho(i + 1,j + 1, p.getCharge() * a * c / cellArea);
-		g.addRho(i+1, j, p.getCharge() * a * d / cellArea);
+		g.addRho(i,		j,		p.getCharge() * b * d);
+		g.addRho(i,		j + 1,	p.getCharge() * b * c);
+		g.addRho(i + 1,	j + 1,	p.getCharge() * a * c);
+		g.addRho(i + 1,	j,		p.getCharge() * a * d);
 	}
 	
 	@Override
