6e5d946b73ccd09f438056defbefa84bb77af78b
Kirill Streltsov <kirill.streltsov@gmail.com>
2013-06-19 15:33:16 +0200

    Cleared anstract Particle class.

--- pixi/src/main/java/org/openpixi/pixi/physics/Particle.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/Particle.java
@@ -23,309 +23,121 @@ import java.io.Serializable;
 
 public abstract class Particle implements Serializable {
 
-	/** x-coordinate */
-	private double x;
-
-	/** y-coordinate */
-	private double y;
-
-	/** radius of particle */
-	private double radius;
-
-	/** velocity in x-direction */
-	private double vx;
-
-	/** velocity in y-direction */
-	private double vy;
-
-	/** acceleration in x-direction */
-	private double ax;
-
-	/** acceleration in y-direction */
-	private double ay;
-
-	/** mass of the particle */
-	private double mass;
-
-	/** electric charge of the particle */
-	private double charge;
-
-	/**previous x position of particle*/
-	private double prevX;
-	/**previous y position of particle*/
-	private double prevY;
-
-	/**charge density of particle for a specific form factor*/
-	private double chargedensity;
-
-	private double Ex;
-	private double Ey;
-	private double Bz;
+	//----------------------------------------------------------------------------------------------
+	// GETTERS
+	//----------------------------------------------------------------------------------------------
 	
-	/**previous position component of the force in x - direction used in Boris & BorisDamped*/
-	private double prevpositionComponentForceX;
+	public double getX() {return 0;}
 	
-	/**previous position component of the force in y - direction used in Boris & BorisDamped*/
-	private double prevpositionComponentForceY;
-
-	/**previous tangent velocity component of the force in x - direction used in Boris*/
-	private double prevtangentVelocityComponentOfForceX;
-
-	/**previous tangent velocity component of the force in y - direction used in Boris*/
-	private double prevtangentVelocityComponentOfForceY;
+	public double getY() {return 0;}
 	
-	/**previous normal velocity component of the force in x - direction used in LeapFrogDamped*/
-	private double prevnormalVelocityComponentOfForceX;
-
-	/**previous normal velocity component of the force in y - direction used in LeapFrogDamped*/
-	private double prevnormalVelocityComponentOfForceY;	
-
-	/**previous magnetic field used in Boris & BorisDamped*/
-	private double prevBz;
+	public double getPrevX() {return 0;}
 	
-	/**previous linear drag coefficient used in BorisDamped*/
-	private double prevLinearDragCoefficient;
-
-	public double getX() {
-		return x;
-	}
-
-	public void setX(double x) {
-		this.x = x;
-	}
-
-	public void addX(double x) {
-		this.x += x;
-	}
-
-	public double getY() {
-		return y;
-	}
-
-	public void setY(double y) {
-		this.y = y;
-	}
-
-	public void addY(double y) {
-		this.y += y;
-	}
-
-	public double getRadius() {
-		return radius;
-	}
-
-	public void setRadius(double radius) {
-		this.radius = radius;
-	}
-
-	public double getVx() {
-		return vx;
-	}
-
-	public void setVx(double vx) {
-		this.vx = vx;
-	}
-
-	public double getVy() {
-		return vy;
-	}
-
-	public void setVy(double vy) {
-		this.vy = vy;
-	}
-
-	public double getAx() {
-		return ax;
-	}
-
-	public void setAx(double ax) {
-		this.ax = ax;
-	}
-
-	public double getAy() {
-		return ay;
-	}
-
-	public void setAy(double ay) {
-		this.ay = ay;
-	}
-
-	public double getMass() {
-		return mass;
-	}
-
-	public void setMass(double mass) {
-		this.mass = mass;
-	}
-
-	public double getCharge() {
-		return charge;
-	}
-
-	public void setCharge(double charge) {
-		this.charge = charge;
-	}
-
-	public double getPrevX() {
-		return prevX;
-	}
-
-	public void setPrevX(double prevX) {
-		this.prevX = prevX;
-	}
-
-	public void addPrevX(double x) {
-		this.prevX += x;
-	}
-
-	public double getPrevY() {
-		return prevY;
-	}
-
-	public void setPrevY(double prevY) {
-		this.prevY = prevY;
-	}
-
-	public void addPrevY(double y) {
-		this.prevY += y;
-	}
-
-	public double getChargedensity() {
-		return chargedensity;
-	}
-
-	public void setChargedensity(double chargedensity) {
-		this.chargedensity = chargedensity;
-	}
-
-	public double getEx() {
-		return Ex;
-	}
-
-	public void setEx(double Ex) {
-		this.Ex = Ex;
-	}
-
-	public double getEy() {
-		return Ey;
-	}
-
-	public void setEy(double Ey) {
-		this.Ey = Ey;
-	}
-
-	public double getBz() {
-		return Bz;
-	}
-
-	public void setBz(double Bz) {
-		this.Bz = Bz;
-	}
-
-	public double getPrevPositionComponentForceX() {
-		return prevpositionComponentForceX;
-	}
-
-	public void setPrevPositionComponentForceX(double prevpositionComponentForceX) {
-		this.prevpositionComponentForceX = prevpositionComponentForceX;
-	}
+	public double getPrevY() {return 0;}
 	
-	public double getPrevPositionComponentForceY() {
-		return prevpositionComponentForceY;
-	}
-
-	public void setPrevPositionComponentForceY(double prevpositionComponentForceY) {
-		this.prevpositionComponentForceY = prevpositionComponentForceY;
-	}
-
+	public double getRadius() {return 0;}
 	
-	public double getPrevTangentVelocityComponentOfForceX() {
-		return prevtangentVelocityComponentOfForceX;
-	}
-
-	public void setPrevTangentVelocityComponentOfForceX(double prevtangentVelocityComponentOfForceX) {
-		this.prevtangentVelocityComponentOfForceX = prevtangentVelocityComponentOfForceX;
-	}
-
-	public double getPrevTangentVelocityComponentOfForceY() {
-		return prevtangentVelocityComponentOfForceY;
-	}
-
-	public void setPrevTangentVelocityComponentOfForceY(double prevtangentVelocityComponentOfForceY) {
-		this.prevtangentVelocityComponentOfForceY = prevtangentVelocityComponentOfForceY;
-	}
+	public double getVx() {return 0;}
 	
-	public double getPrevNormalVelocityComponentOfForceX() {
-		return prevnormalVelocityComponentOfForceX;
-	}
-
-	public void setPrevNormalVelocityComponentOfForceX(double prevnormalVelocityComponentOfForceX) {
-		this.prevnormalVelocityComponentOfForceX = prevnormalVelocityComponentOfForceX;
-	}
-
-	public double getPrevNormalVelocityComponentOfForceY() {
-		return prevnormalVelocityComponentOfForceY;
-	}
-
-	public void setPrevNormalVelocityComponentOfForceY(double prevnormalVelocityComponentOfForceY) {
-		this.prevnormalVelocityComponentOfForceY = prevnormalVelocityComponentOfForceY;
-	}
+	public double getVy() {return 0;}
 	
-	public double getPrevBz() {
-		return prevBz;
-	}
-
-	public void setPrevBz(double prevBz) {
-		this.prevBz = prevBz;
-	}
+	public double getAx() {return 0;}
 	
-	public double getPrevLinearDragCoefficient() {
-		return prevLinearDragCoefficient;
-	}
+	public double getAy() {return 0;}
+	
+	public double getMass() {return 0;}
+	
+	public double getCharge() {return 0;}
+	
+	public double getChargedensity() {return 0;}
+	
+	public double getEx() {return 0;}
+	
+	public double getEy() {return 0;}
+	
+	public double getBz() {return 0;}
+	
+	public double getPrevBz() {return 0;}
+	
+	public double getPrevPositionComponentForceX() {return 0;}
+	
+	public double getPrevPositionComponentForceY() {return 0;}
+	
+	public double getPrevTangentVelocityComponentOfForceX() {return 0;}
+	
+	public double getPrevTangentVelocityComponentOfForceY() {return 0;}
+	
+	public double getPrevNormalVelocityComponentOfForceX() {return 0;}
+	
+	public double getPrevNormalVelocityComponentOfForceY() {return 0;}
+	
+	public double getPrevLinearDragCoefficient() {return 0;}
 
-	public void setPrevLinearDragCoefficient(double prevLinearDragCoefficient) {
-		this.prevLinearDragCoefficient = prevLinearDragCoefficient;
-	}
+	//----------------------------------------------------------------------------------------------
+	// SETTERS
+	//----------------------------------------------------------------------------------------------
+	
+	public void setX(double x) {}
+
+	public void addX(double x) {}
+
+	public void setY(double y) {}
+
+	public void addY(double y) {}
+
+	public void setRadius(double radius) {}
+
+	public void setVx(double vx) {}
+
+	public void setVy(double vy) {}
+	
+	public void setAx(double ax) {}
+
+	public void setAy(double ay) {}
+
+	public void setMass(double mass) {}
+
+	public void setCharge(double charge) {}
+
+	public void setPrevX(double prevX) {}
+
+	public void addPrevX(double x) {}
+
+	public void setPrevY(double prevY) {}
+
+	public void addPrevY(double y) {}
+
+	public void setChargedensity(double chargedensity) {}
+
+	public void setEx(double Ex) {}
+
+	public void setEy(double Ey) {}
+
+	public void setBz(double Bz) {}
+	
+	public void setPrevBz(double prevBz) {}
+
+	public void setPrevPositionComponentForceX(double argument) {}
+
+	public void setPrevPositionComponentForceY(double argument) {}
+
+	public void setPrevTangentVelocityComponentOfForceX(double argument) {}
+
+	public void setPrevTangentVelocityComponentOfForceY(double argument) {}
+
+	public void setPrevNormalVelocityComponentOfForceX(double argument) {}
+
+	public void setPrevNormalVelocityComponentOfForceY(double argument) {}
+
+	public void setPrevLinearDragCoefficient(double argument) {}
 
 
-	/** Empty constructor */
-	public Particle() {
-	}
-
-	/** Copy constructor */
-	public Particle(Particle p) {
-		x = p.x;
-		y = p.y;
-		radius = p.radius;
-		vx = p.vx;
-		vy = p.vy;
-		ax = p.ax;
-		ay = p.ay;
-		mass = p.mass;
-		charge = p.charge;
-		prevX = p.prevX;
-		prevY = p.prevY;
-		chargedensity = p.chargedensity;
-		Ex = p.Ex;
-		Ey = p.Ey;
-		Bz = p.Bz;
-		prevpositionComponentForceX = p.prevpositionComponentForceX;
-		prevpositionComponentForceY = p.prevpositionComponentForceY;
-		prevtangentVelocityComponentOfForceX = p.prevtangentVelocityComponentOfForceX;
-		prevtangentVelocityComponentOfForceY = p.prevtangentVelocityComponentOfForceY;
-		prevBz = p.prevBz;
-		prevLinearDragCoefficient = p.prevLinearDragCoefficient;
-	}
-
-
-	public void storePosition() {
-		prevX = x;
-		prevY = y;
-	}
-
+	//----------------------------------------------------------------------------------------------
+	// UTILITY METHODS
+	//----------------------------------------------------------------------------------------------
+	
+	public void storePosition() {}
 
 	@Override
-	public String toString() {
-		return String.format("[%.3f,%.3f]", x, y);
-	}
+	public String toString() {return null;}
 }
