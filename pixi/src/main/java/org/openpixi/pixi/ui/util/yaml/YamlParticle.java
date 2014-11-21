189ceaa99b9f85ca8ef5df27ef34a4aeace52057
Andreas Ipp <andreas.ipp@gmail.com>
2014-11-21 18:22:40 +0100

    New YAML setting for pair of particles.
    Add YamlParticle class to create new particles from a YAML description.

---
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/YamlParticle.java
@@ -0,0 +1,50 @@
+package org.openpixi.pixi.ui.util.yaml;
+
+import org.openpixi.pixi.physics.Settings;
+import org.openpixi.pixi.physics.particles.ParticleFull;
+
+public class YamlParticle {
+	public Double x;
+	public Double y;
+	public Double r;
+	public Double vx;
+	public Double vy;
+	public Double m;
+	public Double q;
+
+	public void applyTo(Settings settings) {
+
+		ParticleFull p = new ParticleFull();
+
+		if (x != null) {
+			p.setX(x);
+		}
+
+		if (y != null) {
+			p.setY(y);
+		}
+
+		if (r != null) {
+			p.setRadius(r);
+		}
+
+		if (vx != null) {
+			p.setVx(vx);
+		}
+
+		if (vy != null) {
+			p.setVy(vy);
+		}
+
+		if (m != null) {
+			p.setMass(m);
+		}
+
+		if (q != null) {
+			p.setCharge(q);
+		}
+
+		settings.addParticle(p);
+	}
+
+}
--- pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/YamlSettings.java
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/YamlSettings.java
@@ -1,26 +1,60 @@
 package org.openpixi.pixi.ui.util.yaml;
 
+import java.util.List;
+
+import org.openpixi.pixi.physics.GeneralBoundaryType;
 import org.openpixi.pixi.physics.Settings;
+import org.openpixi.pixi.physics.fields.PoissonSolverFFTPeriodic;
+import org.openpixi.pixi.physics.fields.SimpleSolver;
+import org.openpixi.pixi.physics.grid.ChargeConservingCIC;
 
 /**
  * Generic settings class into which the YAML parser parses
  */
 public class YamlSettings {
-	public Double simulationWidth;
-	public Double simulationHeight;
-	public Integer numberOfParticles;
+	public Double timeStep;
+	public Double speedOfLight;
+	public Integer gridStep;
+	public Integer gridCellsX;
+	public Integer gridCellsY;
+	public List<YamlParticle> particles;
 	
 	public void applyTo(Settings settings) {
-		if (simulationWidth != null) {
-			settings.setSimulationWidth(simulationWidth);
+
+		// Default settings:
+		settings.setRelativistic(true);
+		settings.setBoundary(GeneralBoundaryType.Periodic);
+		settings.setGridSolver(new SimpleSolver());
+		settings.setPoissonSolver(new PoissonSolverFFTPeriodic());
+		settings.useGrid(true);
+		settings.setInterpolator(new ChargeConservingCIC());
+
+		// Custom settings:
+		if (timeStep != null) {
+			settings.setTimeStep(timeStep);
 		}
 
-		if (simulationHeight != null) {
-			settings.setSimulationHeight(simulationHeight);
+		if (speedOfLight != null) {
+			settings.setSpeedOfLight(speedOfLight);
 		}
 
-		if (numberOfParticles != null) {
-			settings.setNumOfParticles(numberOfParticles);
+		if (gridStep != null) {
+			settings.setGridStep(gridStep);
 		}
+
+		if (gridCellsX != null) {
+			settings.setGridCellsX(gridCellsX);
+		}
+
+		if (gridCellsY != null) {
+			settings.setGridCellsY(gridCellsY);
+		}
+
+		if (particles != null) {
+			for (YamlParticle p : particles) {
+				p.applyTo(settings);
+			}
+		}
+
 	}
 }
