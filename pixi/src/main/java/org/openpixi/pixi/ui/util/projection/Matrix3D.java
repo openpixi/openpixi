9747f73efc174b48b8c373193a1fbcf5055e20a7
Andreas Ipp <andreas.ipp@gmail.com>
2015-01-20 10:43:03 +0100

    Add new class Matrix3D.

---
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/projection/Matrix3D.java
@@ -0,0 +1,43 @@
+package org.openpixi.pixi.ui.util.projection;
+
+/**
+ * Simple 3D matrix class.
+ * Avoid overhead of lists.
+ *
+ */
+public class Matrix3D {
+
+	public double m11, m12, m13;
+	public double m21, m22, m23;
+	public double m31, m32, m33;
+
+	public Matrix3D() {
+	}
+
+	public void reset() {
+		m11 = 0;
+		m12 = 0;
+		m13 = 0;
+		m21 = 0;
+		m22 = 0;
+		m23 = 0;
+		m31 = 0;
+		m32 = 0;
+		m33 = 0;
+	}
+
+	public Matrix3D multiply(Matrix3D B) {
+		Matrix3D A = this;
+		Matrix3D C = new Matrix3D();
+		C.m11 = A.m11 * B.m11 + A.m12 * B.m21 + A.m13 * B.m31;
+		C.m12 = A.m11 * B.m12 + A.m12 * B.m22 + A.m13 * B.m32;
+		C.m13 = A.m11 * B.m13 + A.m12 * B.m23 + A.m13 * B.m33;
+		C.m21 = A.m21 * B.m11 + A.m22 * B.m21 + A.m23 * B.m31;
+		C.m22 = A.m21 * B.m12 + A.m22 * B.m22 + A.m23 * B.m32;
+		C.m23 = A.m21 * B.m13 + A.m22 * B.m23 + A.m23 * B.m33;
+		C.m31 = A.m31 * B.m11 + A.m32 * B.m21 + A.m33 * B.m31;
+		C.m32 = A.m31 * B.m12 + A.m32 * B.m22 + A.m33 * B.m32;
+		C.m33 = A.m31 * B.m13 + A.m32 * B.m23 + A.m33 * B.m33;
+		return C;
+	}
+}
