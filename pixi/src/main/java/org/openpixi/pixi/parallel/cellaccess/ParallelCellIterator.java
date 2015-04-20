ef80508b554793555b7206966e8df8dc28bb782b
= <david.mueller@tuwien.ac.at>
2015-04-20 17:24:37 +0200

    ParallelCellIterator should now work in 3D. This time I mean it!

--- pixi/src/main/java/org/openpixi/pixi/parallel/cellaccess/ParallelCellIterator.java
+++ pixi/src/main/java/org/openpixi/pixi/parallel/cellaccess/ParallelCellIterator.java
@@ -84,8 +84,8 @@ public class ParallelCellIterator extends CellIterator {
             pos[0] = ci % dimensions.xsize() + dimensions.xmin();
             ci = (ci - pos[0]) / dimensions.xsize();
             pos[1] = ci % dimensions.xsize() + dimensions.ymin();
-            ci = (ci - pos[1]) / dimensions.ysize();
-            pos[2] = ci % dimensions.ysize() + dimensions.zmin();
+            ci = (ci - pos[1]) / dimensions.zsize();
+            pos[2] = ci % dimensions.zsize() + dimensions.zmin();
 
             return pos;
         }
