e654f6a3ea5244a44de80f8732b72a0dd9c5b50d
David Müller <david.mueller@tuwien.ac.at>
2015-05-05 13:38:04 +0200

    More documentation of the Grid class.

--- pixi/src/main/java/org/openpixi/pixi/physics/grid/Grid.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/grid/Grid.java
@@ -8,30 +8,47 @@ import org.openpixi.pixi.physics.fields.FieldSolver;
 public class Grid {
 
 	/**
-	 * solver algorithm for the field equations
+	 * Solver algorithm for the field equations
 	 */
 	private FieldSolver fsolver;
+
+	/**
+	 * Instance of the CellIterator which iterates over all cells in the grid (either sequentially or in parallel)
+	 */
 	private CellIterator cellIterator;
+
+	/*
+	 *      Cell actions
+	 */
 	private ResetChargeAction resetCharge = new ResetChargeAction();
 	private ResetCurrentAction resetCurrent = new ResetCurrentAction();
 	private StoreFieldsAction storeFields = new StoreFieldsAction();
+	/**
+	 * Cell array. This one dimensional array is used to represent the d-dimensional grid. The cells are indexed by
+	 * their cell ids. Cell ids can be computed from lattice coordinates with the index() method.
+	 */
 	private Cell[] cells;
+
 	/**
 	 * Number of dimensions
 	 */
 	private int numDim;
+
 	/**
 	 * Number of colors
 	 */
 	private int numCol;
+
 	/**
-	 * Number of cells in d dimension
+	 * Number of cells (size of the grid) in each direction
 	 */
 	private int numCells[];
+
 	/**
 	 * Spatial lattice spacing
 	 */
 	private double as;
+
 	/**
 	 * Gauge coupling strength
 	 */
@@ -42,52 +59,113 @@ public class Grid {
 	 */
 	private int[][] unitVectors;
 
+	/**
+	 * Returns the FieldSolver instance currently used.
+	 * @return  Instance of the FieldSolver
+	 */
 	public FieldSolver getFsolver() {
 		return fsolver;
 	}
 
+	/**
+	 * Sets the FieldSolver used for grid updates.
+	 * @param fsolver   Instance of the FieldSolver which should be used.
+	 */
 	public void setFsolver(FieldSolver fsolver) {
 		this.fsolver = fsolver;
 	}
 
+	/**
+	 * Returns the YMField instance of the (dir)-component of the current.
+	 * @param coor      Lattice coordinate of the current
+	 * @param dir       Index of the component
+	 * @return          YMField instance of the (dir)-component of the current.
+	 */
 	public YMField getJ(int[] coor, int dir) {
 		return cells[index(coor)].getJ(dir);
 	}
 
+	/**
+	 * Adds a YMField instance to the (dir)-component of the current.
+	 * @param coor      Lattice coordinate of the current
+	 * @param dir       Index of the component
+	 * @param field     YMField to be added to the (dir)-component of the current.
+	 */
 	public void addJ(int[] coor, int dir, YMField field) {
 		cells[index(coor)].addJ(dir, field);
 	}
-	
+
+	/**
+	 * Returns the YMField instance of the charge density.
+	 * @param coor      Lattice coordinate of the charge density
+	 * @return          YMField instance of the charge density
+	 */
 	public YMField getRho(int[] coor) {
 		return cells[index(coor)].getRho();
 	}
-	
+
+	/**
+	 * Sets the YMField instance of the charge density.
+	 * @param coor      Lattice coordinate of the charge density
+	 * @param field     YMField instance which the electric field should be set to.
+	 */
 	public void setRho(int[] coor, YMField field) {
 		cells[index(coor)].setRho(field);
 	}
-	
+
+	/**
+	 * Adds a YMField to the charge density.
+	 * @param coor      Lattice coordinate of the charge density
+	 * @param field     YMField instance which should be added.
+	 */
 	public void addRho(int[] coor, YMField field) {
 		cells[index(coor)].addRho(field);
 	}
 
+	/**
+	 * Returns the YMField instance of the (dir)-component of the electric field.
+	 * @param coor      Lattice coordinate of the electric field
+	 * @param dir       Index of the component
+	 * @return          YMField instance of the (dir)-component
+	 */
 	public YMField getE(int[] coor, int dir) {
 		return cells[index(coor)].getE(dir);
 	}
 
+	/**
+	 * Sets the YMField instance of the (dir)-component of the electric field.
+	 * @param coor      Lattice coordinate of the electric field
+	 * @param dir       Index of the component
+	 * @param field     YMField instance which the electric field should be set to.
+	 */
 	public void setE(int[] coor, int dir, YMField field) {
 		cells[index(coor)].setE(dir, field);
 	}
-	
+
+	/**
+	 * Adds a YMField to the (dir)-component of the electric field.
+	 * @param coor      Lattice coordinate of the electric field
+	 * @param dir       Index of the component
+	 * @param field     YMField instance which should be added.
+	 */
 	public void addE(int[] coor, int dir, YMField field) {
 		cells[index(coor)].addE(dir, field);
 	}
 
+
+	/**
+	 * Returns the (dir1, dir2)-component of the field strength tensor at a certain lattice coordinate.
+	 * @param coor      Lattice coordinate of the field strength tensor
+	 * @param dir1      First spatial component (0 - (numberOfDimensions-1))
+	 * @param dir2      Second spatial component (0 - (numberOfDimensions-1))
+	 * @return          YMField instance of the (dir1, dir2)-component
+	 */
 	public YMField getFTensor(int[] coor, int dir1, int dir2) {
 		return cells[index(coor)].getFieldStrength(dir1, dir2);
 	}
 
 	/**
-	 * Sets value the field strength component (dir1, dir2) tensor at a certain lattice coordinate.
+	 * Sets the (dir1, dir2)-component of the field strength tensor at a certain lattice coordinate.
 	 * @param coor      Lattice coordinate of the field strength tensor
 	 * @param dir1      First space component (0 - (numberOfDimensions-1))
 	 * @param dir2      Second space component (0 - (numberOfDimensions-1))
@@ -98,7 +176,7 @@ public class Grid {
 	}
 
 	/**
-	 * Returns the gauge link at (t) at a given lattice coordinate in a given direction/
+	 * Returns the gauge link at time (t) at a given lattice coordinate in a given direction.
 	 * @param coor  Lattice coordinate of the gauge link
 	 * @param dir   Direction of the gauge link
 	 * @return      Instance of the gauge link
@@ -108,7 +186,7 @@ public class Grid {
 	}
 
 	/**
-	 * Sets the gauge link at (t) at given lattice coordinate in given direction to a new value.
+	 * Sets the gauge link at time (t) at given lattice coordinate in given direction to a new value.
 	 * @param coor  Lattice coordinate of the gauge link
 	 * @param dir   Direction of the gauge link
 	 * @param mat   LinkMatrix instance
@@ -118,7 +196,7 @@ public class Grid {
 	}
 
 	/**
-	 * Returns the gauge link at (t+dt) at a given lattice coordinate in a given direction/
+	 * Returns the gauge link at time (t+dt) at a given lattice coordinate in a given direction.
 	 * @param coor  Lattice coordinate of the gauge link
 	 * @param dir   Direction of the gauge link
 	 * @return      Instance of the gauge link
@@ -128,7 +206,7 @@ public class Grid {
 	}
 
 	/**
-	 * Sets the gauge link at (t+dt) at given lattice coordinate in given direction to a new value.
+	 * Sets the gauge link at time (t+dt) at given lattice coordinate in given direction to a new value.
 	 * @param coor  Lattice coordinate of the gauge link
 	 * @param dir   Direction of the gauge link
 	 * @param mat   LinkMatrix instance
@@ -163,7 +241,7 @@ public class Grid {
 	}
 
 	/**
-	 * Returns the Cell instance at given lattice coorindates with respect to periodic boundary conditions.
+	 * Returns the Cell instance at given lattice coordinates with respect to periodic boundary conditions.
 	 *
 	 * @param coor  Lattice coordinate of the cell
 	 * @return      Cell instance at lattice coordinates with respect to periodic boundary conditions
@@ -173,7 +251,7 @@ public class Grid {
 	}
 
 	/**
-	 * Returns full Cell array of the grid. Note that the cells in this array are index by cell indices.
+	 * Returns full Cell array of the grid. Note that the cells in this array are indexed by cell ids.
 	 * @return  Full array with all cells
 	 */
 	public Cell[] getCells() {
@@ -181,8 +259,9 @@ public class Grid {
 	}
 
 	/**
-	 * Main constructor for the Grid class. Given a settings file it initializes the lattice and sets up the FieldSolver and the CellIterator.
-	 * @param settings
+	 * Main constructor for the Grid class. Given a settings file it initializes the lattice and sets up the FieldSolver
+	 * and the CellIterator.
+	 * @param settings  Settings instance
 	 */
 	public Grid(Settings settings) {
 
@@ -207,19 +286,7 @@ public class Grid {
 	}
 
 	/**
-	 * Change the size of the grid. TODO make sure the method can not be called
-	 * in distributed version E.g. throw an exception if this is distributed
-	 * version
-	 */
-	public void changeSize(int[] NewNumberOfCells) {
-		numCells = NewNumberOfCells;
-		createGrid();
-		fsolver.changeSize(numCells);
-		cellIterator.setNormalMode(numCells);
-	}
-
-	/**
-	 * This methods initializes the each cell in the grid.
+	 * This methods initializes each cell in the grid.
 	 */
 	private void createGrid() {
 
@@ -244,8 +311,8 @@ public class Grid {
 
 	/**
 	 * This method advances the grid by one time step:
-	 * It stores the fields (i.e. sets (t+dt) fields from the last grid update to the old ones)
-	 * and calls the FieldSolver to solve the equations of motion for one time step.
+	 * It stores the fields, i.e. sets (t+dt) fields from the last grid update to the old ones at time (t) and calls the
+	 * FieldSolver to solve the equations of motion for one time step.
 	 *
 	 * @param tstep size of the time step
 	 */
@@ -319,7 +386,9 @@ public class Grid {
 	}
 
 	/**
-	 * Getter for gauge links. Returns a link starting from a certain lattice coordinate with the right direction and orientation.
+	 * Getter for gauge links. Returns a link starting from a certain lattice coordinate with the right direction and
+	 * orientation.
+	 *
 	 * Examples:
 	 * Link starting at coor in positive x-direction: getLink(coor, 0, 1)
 	 * Link starting at coor in negative x-direction: getLink(coor, 0, -1)
@@ -340,8 +409,8 @@ public class Grid {
 
 
 	/**
-	 * This method translates a lattice coordinate vector to the corresponding cell id with respect
-	 * to periodic boundary conditions.
+	 * This method translates a lattice coordinate vector to the corresponding cell id with respect to periodic boundary
+	 * conditions.
 	 *
 	 * @param coor  lattice coordinate vector
 	 * @return      cell id
@@ -358,8 +427,8 @@ public class Grid {
 	}
 
 	/**
-	 * This method implements periodic boundary conditions on the lattice.
-	 * If the lattice coordinate vector is outside the bounds of the simulation box it gets shifted appropriately.
+	 * This method implements periodic boundary conditions on the lattice. If the lattice coordinate vector is outside
+	 * the bounds of the simulation box it gets shifted appropriately.
 	 *
 	 * @param coor  lattice coordinate vector
 	 * @return      shifted lattice coordinate vector
@@ -374,7 +443,9 @@ public class Grid {
 	}
 
 	/**
-	 * Shifts a lattice coorindate vector by one unit step in a certain direction. The direction is passed as an integer for the direction and an orientation.
+	 * Shifts a lattice coorindate vector by one unit step in a certain direction. The direction is passed as an integer
+	 * for the direction and an orientation.
+	 *
 	 * Examples:
 	 * Shift by one in negative x-direction: shift(coor, 0, -1)
 	 * Shift by one in positive x-direction: shift(coor, 0, 1)
@@ -398,6 +469,13 @@ public class Grid {
 		return periodic(shiftedCoordinate);
 	}
 
+	/*
+		Cell actions
+	 */
+
+	/**
+	 * ResetCurrentAction is used by the CellIterator to reset all currents in the grid.
+	 */
 	private class ResetCurrentAction implements CellAction {
 
 		public void execute(Grid grid, int[] coor) {
@@ -405,6 +483,9 @@ public class Grid {
 		}
 	}
 
+	/**
+	 * ResetChargeAction is used by the CellIterator to reset all charges on the grid.
+	 */
 	private class ResetChargeAction implements CellAction {
 
 		public void execute(Grid grid, int[] coor) {
@@ -412,6 +493,10 @@ public class Grid {
 		}
 	}
 
+	/**
+	 * StoreFieldsAction is used by the CellIterator to save the new (t+dt) values of the fields to the variables of the
+	 * old (t) fields. Or more simple: The new fields become the old fields.
+	 */
 	private class StoreFieldsAction implements CellAction {
 
 		public void execute(Grid grid, int[] coor) {
