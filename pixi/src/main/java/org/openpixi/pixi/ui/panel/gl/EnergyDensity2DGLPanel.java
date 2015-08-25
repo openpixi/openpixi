d543327462ab1c117fc3be97831b0a27a6fa32cd
Andreas Ipp <andreas.ipp@gmail.com>
2015-08-25 15:59:31 +0200

    Avoid unwanted artifacts in graphical display
    by switching from int to float variables.

--- pixi/src/main/java/org/openpixi/pixi/ui/panel/gl/EnergyDensity2DGLPanel.java
+++ pixi/src/main/java/org/openpixi/pixi/ui/panel/gl/EnergyDensity2DGLPanel.java
@@ -18,10 +18,6 @@
  */
 package org.openpixi.pixi.ui.panel.gl;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
@@ -77,11 +73,11 @@ public class EnergyDensity2DGLPanel extends AnimationGLPanel {
 			gl2.glBegin( GL2.GL_QUAD_STRIP );
 			for(int k = 0; k < s.grid.getNumCells(1); k++)
 			{
-				int xstart = (int) (s.grid.getLatticeSpacing() * (i + 0.5) * sx);
-				int xstart2 = (int)(s.grid.getLatticeSpacing() * i * sx);
-				int xstart3 = (int)(s.grid.getLatticeSpacing() * (i + 1) * sx);
-				int ystart = (int) (s.grid.getLatticeSpacing() * (k + 0.5) * sy);
-				int ystart2 = (int) (s.grid.getLatticeSpacing() * k * sy);
+				//float xstart = (float) (s.grid.getLatticeSpacing() * (i + 0.5) * sx);
+				float xstart2 = (float)(s.grid.getLatticeSpacing() * i * sx);
+				float xstart3 = (float)(s.grid.getLatticeSpacing() * (i + 1) * sx);
+				//float ystart = (float) (s.grid.getLatticeSpacing() * (k + 0.5) * sy);
+				float ystart2 = (float) (s.grid.getLatticeSpacing() * k * sy);
 
 				pos[0] = i;
 				pos[1] = k;
