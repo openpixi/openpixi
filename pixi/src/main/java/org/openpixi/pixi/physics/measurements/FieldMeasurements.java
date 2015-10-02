6826d889102e0854a76d9f0b32ffbc6b7d5c2184
David Mueller <david.mueller@tuwien.ac.at>
2015-10-02 13:17:21 +0200

    The FieldMeasurements class now supports excluded regions. This can be used for CGC collisions in temporal gauge where the boundaries have to be fixed. Since periodic boundary conditions are used in the simulation, the boundary regions have to be excluded from calculations to get correct results.

--- pixi/src/main/java/org/openpixi/pixi/physics/measurements/FieldMeasurements.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/measurements/FieldMeasurements.java
@@ -1,17 +1,48 @@
 package org.openpixi.pixi.physics.measurements;
 
 import org.openpixi.pixi.math.AlgebraElement;
+import org.openpixi.pixi.physics.grid.Cell;
 import org.openpixi.pixi.physics.grid.Grid;
 import org.openpixi.pixi.parallel.cellaccess.CellAction;
 import org.openpixi.pixi.physics.util.GridFunctions;
 
 
 public class FieldMeasurements {
-	
-	EFieldSquared Esquared = new EFieldSquared();
-	BFieldSquared Bsquared = new BFieldSquared();
-	GaussLaw GaussConstraint = new GaussLaw();
-	TotalCharge totalCharge = new TotalCharge();
+
+	private EFieldSquared Esquared;
+	private BFieldSquared Bsquared;
+	private GaussLaw GaussConstraint;
+	private TotalCharge totalCharge;
+
+	private boolean useExcludedBoundaries;
+	private boolean[] excludedRegion;
+
+	/**
+	 * Empty constructor for standard usage.
+	 */
+	public FieldMeasurements() {
+		this.useExcludedBoundaries = false;
+
+		Esquared = new EFieldSquared();
+		Bsquared = new BFieldSquared();
+		GaussConstraint = new GaussLaw();
+		totalCharge = new TotalCharge();
+	}
+
+	/**
+	 * Alternative constructor for use with non-periodic boundaries.
+	 * @param excludedRegion
+	 */
+	public FieldMeasurements(boolean[] excludedRegion) {
+		this.useExcludedBoundaries = true;
+
+		Esquared = new EFieldSquared(excludedRegion);
+		Bsquared = new BFieldSquared(excludedRegion);
+		GaussConstraint = new GaussLaw(excludedRegion);
+		totalCharge = new TotalCharge(excludedRegion);
+
+		this.excludedRegion = excludedRegion;
+	}
 
 	public double calculateEsquared(Grid grid) {
 		Esquared.reset();
@@ -49,9 +80,35 @@ public class FieldMeasurements {
 		return totalCharge.getSum(grid);
 	}
 
-	private class EFieldSquared implements CellAction {
+	private class FieldMeasurementAction implements CellAction {
+		protected boolean useExcludedRegion;
+		protected boolean[] excludedRegion;
+
+		public FieldMeasurementAction() {
+			this.useExcludedRegion = false;
+		}
+
+		public FieldMeasurementAction(boolean[] excludedRegion) {
+			this.useExcludedRegion = true;
+			this.excludedRegion = excludedRegion;
+		}
+
+		public void execute(Grid grid, int index) {
+
+		}
+	}
+
+	private class EFieldSquared extends FieldMeasurementAction {
 
 		private double[] sum;
+
+		public EFieldSquared() {
+			super();
+		}
+
+		public EFieldSquared(boolean[] excludedRegion) {
+			super(excludedRegion);
+		}
 
         public void reset() {
         	sum = new double[3];//TODO Make this method d-dimensional!!
@@ -68,23 +125,33 @@ public class FieldMeasurements {
         }
 
         public void execute(Grid grid, int index) {
-			int numDir = grid.getNumberOfDimensions();
-			double[] res = new double[numDir];
-			for (int i = 0; i < numDir; i++) {
-				res[i] += grid.getE(index, i).square();
-				//res += grid.getEsquaredFromLinks(coor, i);
-			}
-			synchronized(this) {
+			if(!excludedRegion[index]) {
+				int numDir = grid.getNumberOfDimensions();
+				double[] res = new double[numDir];
 				for (int i = 0; i < numDir; i++) {
-					sum[i] += res[i];   	// Synchronisierte Summenbildung
+					res[i] += grid.getE(index, i).square();
+					//res += grid.getEsquaredFromLinks(coor, i);
+				}
+				synchronized (this) {
+					for (int i = 0; i < numDir; i++) {
+						sum[i] += res[i];    // Synchronisierte Summenbildung
+					}
 				}
 			}
 		}
 	}
 
-	private class BFieldSquared implements CellAction {
+	private class BFieldSquared extends FieldMeasurementAction {
 
 		private double[] sum;
+
+		public BFieldSquared() {
+			super();
+		}
+
+		public BFieldSquared(boolean[] excludedRegion) {
+			super(excludedRegion);
+		}
 
         public void reset() {
         	sum = new double[3];
@@ -102,25 +169,35 @@ public class FieldMeasurements {
 		}
         
         public void execute(Grid grid, int index) {
-			int numDir = grid.getNumberOfDimensions();
-			double[] res = new double[numDir];
-			for (int i = 0; i < numDir; i++) {
-				//res += grid.getB(coor, i).square();
-				// Averaging B(-dt/2) and B(dt/2) to approximate B(0).
-				res[i] += 0.5 * (grid.getBsquaredFromLinks(index, i, 0) + grid.getBsquaredFromLinks(index, i, 1));
-			}
-
-			synchronized(this) {
+			if(!excludedRegion[index]) {
+				int numDir = grid.getNumberOfDimensions();
+				double[] res = new double[numDir];
 				for (int i = 0; i < numDir; i++) {
-					sum[i] += res[i];   	// Synchronisierte Summenbildung
+					//res += grid.getB(coor, i).square();
+					// Averaging B(-dt/2) and B(dt/2) to approximate B(0).
+					res[i] += 0.5 * (grid.getBsquaredFromLinks(index, i, 0) + grid.getBsquaredFromLinks(index, i, 1));
+				}
+
+				synchronized (this) {
+					for (int i = 0; i < numDir; i++) {
+						sum[i] += res[i];    // Synchronisierte Summenbildung
+					}
 				}
 			}
 		}
 	}
 	
-	private class GaussLaw implements CellAction {
+	private class GaussLaw extends FieldMeasurementAction {
 
 		private double sum;
+
+		public GaussLaw() {
+			super();
+		}
+
+		public GaussLaw(boolean[] excludedRegion) {
+			super(excludedRegion);
+		}
 
         public void reset() {
         	sum = 0.0;
@@ -132,16 +209,26 @@ public class FieldMeasurements {
         }
         
         public void execute(Grid grid, int index) {
-			double result = grid.getGaussConstraintSquared(index);
-			synchronized(this) {
-			       sum += result;   // Synchronisierte Summenbildung
+			if(!excludedRegion[index]) {
+				double result = grid.getGaussConstraintSquared(index);
+				synchronized (this) {
+					sum += result;   // Synchronisierte Summenbildung
+				}
 			}
 		}
 	}
 
-	private class TotalCharge implements CellAction {
+	private class TotalCharge extends FieldMeasurementAction {
 
 		private AlgebraElement charge;
+
+		public TotalCharge() {
+			super();
+		}
+
+		public TotalCharge(boolean[] excludedRegion) {
+			super(excludedRegion);
+		}
 
 		public void reset(Grid grid) {
 			charge = grid.getElementFactory().algebraZero();
@@ -154,8 +241,10 @@ public class FieldMeasurements {
 		}
 
 		public void execute(Grid grid, int index) {
-			synchronized(this) {
-				charge.addAssign(grid.getRho(index));   // Synchronisierte Summenbildung
+			if(!excludedRegion[index]) {
+				synchronized (this) {
+					charge.addAssign(grid.getRho(index));   // Synchronisierte Summenbildung
+				}
 			}
 		}
 	}
