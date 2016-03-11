1e20149b11758460f704d71ebe03ebe44f2477b9
David Mueller <david.mueller@tuwien.ac.at>
2016-03-11 17:59:48 +0100

    Preliminary multithreaded version of the Poisson solver. Note sure if it is really much faster.

--- pixi/src/main/java/org/openpixi/pixi/physics/fields/NewLCPoissonSolver.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/NewLCPoissonSolver.java
@@ -3,6 +3,7 @@ package org.openpixi.pixi.physics.fields;
 import org.openpixi.pixi.math.AlgebraElement;
 import org.openpixi.pixi.math.ElementFactory;
 import org.openpixi.pixi.math.GroupElement;
+import org.openpixi.pixi.parallel.cellaccess.CellAction;
 import org.openpixi.pixi.physics.Simulation;
 import org.openpixi.pixi.physics.gauge.DoubleFFTWrapper;
 import org.apache.commons.math3.special.Erf;
@@ -115,6 +116,11 @@ public class NewLCPoissonSolver {
 		}
 
 		// Second step: compute links from transversal potential
+		GaugeLinkSetter gaugeLinkSetter = new GaugeLinkSetter();
+		gaugeLinkSetter.initialize(s.grid, gridCopy, phi);
+		s.grid.getCellIterator().execute(s.grid, gaugeLinkSetter);
+
+		/*
 		double gaugeNorm = g * as;
 		int totalCells = s.grid.getTotalNumberOfCells();
 		for (int i = 0; i < totalCells; i++) {
@@ -156,17 +162,30 @@ public class NewLCPoissonSolver {
 				}
 			}
 		}
+		*/
 
 
 		// Third step: Compute electric field from temporal plaquette
+		ElectricFieldSetter electricFieldSetter = new ElectricFieldSetter();
+		s.grid.getCellIterator().execute(s.grid, electricFieldSetter);
+		gridCopy.getCellIterator().execute(gridCopy, electricFieldSetter);
+
+		/*
 		for (int i = 0; i < totalCells; i++) {
 			for (int j = 0; j < numberOfDimensions; j++) {
 				s.grid.setE(i, j, s.grid.getEFromLinks(i, j));
 				gridCopy.setE(i, j, gridCopy.getEFromLinks(i, j));
 			}
 		}
+		*/
 
 		// Compute gauss violation from grid copy
+		GaussViolationCalculation gvCalculation = new GaussViolationCalculation();
+		gvCalculation.reset(gridCopy);
+		gridCopy.getCellIterator().execute(gridCopy, gvCalculation);
+		gaussViolation = gvCalculation.getResult();
+
+		/*
 		gaussViolation = new AlgebraElement[s.grid.getTotalNumberOfCells()];
 		for (int i = 0; i < gridCopy.getTotalNumberOfCells(); i++) {
 			if(s.grid.isActive(i)) {
@@ -175,6 +194,7 @@ public class NewLCPoissonSolver {
 				gaussViolation[i] = factory.algebraZero();
 			}
 		}
+		*/
 	}
 
 	public AlgebraElement getGaussConstraint(int i) {
@@ -225,5 +245,99 @@ public class NewLCPoissonSolver {
 		}
 
 		return momentumSquared / (as * as);
+	}
+
+	// Classes for multithreaded operations
+
+	/**
+	 * This class sets all the gauge links according to the solution of the Poisson equation.
+	 */
+	private class GaugeLinkSetter implements CellAction {
+		private Grid grid;
+		private Grid gridCopy;
+		private AlgebraElement[] phi;
+
+		public void initialize(Grid grid, Grid gridCopy, AlgebraElement[] phi) {
+			this.grid = grid;
+			this.gridCopy = gridCopy;
+			this.phi = phi;
+		}
+
+		public void execute(Grid gr, int index) {
+			int[] gridPos = this.grid.getCellPos(index);
+			int[] transversalGridPos = GridFunctions.reduceGridPos(gridPos, direction);
+			int longitudinalGridPos = gridPos[direction];
+			double z = longitudinalGridPos * as - location;
+			int transversalCellIndex = GridFunctions.getCellIndex(transversalGridPos, transversalNumCells);
+
+			// Shape function (i.e. F(z,t)) at t = -dt/2 and t = dt /2.
+			double s0 = g * integratedShapeFunction(z, - at / 2.0, orientation, longitudinalWidth);
+			double s1 = g * integratedShapeFunction(z, + at / 2.0, orientation, longitudinalWidth);
+
+			// Setup the gauge links at t = -dt/2 and t = dt/2
+			GroupElement V0 = this.phi[transversalCellIndex].mult(- s0).getLink();
+			GroupElement V0next = this.phi[transversalCellIndex].mult(- s1).getLink();
+
+			// New method: Apply gauge transformation directly to gauge links without the use of a discretized derivative.
+			for (int j = 0; j < numberOfDimensions; j++) {
+				if (j != direction) {
+					int transversalCellIndexShifted = GridFunctions.getCellIndex(
+							GridFunctions.reduceGridPos(
+									this.grid.getCellPos(s.grid.shift(index, j, 1))
+									, direction),
+							transversalNumCells);
+
+					GroupElement V1 = this.phi[transversalCellIndexShifted].mult(- s0).getLink();
+					GroupElement V1next = this.phi[transversalCellIndexShifted].mult(- s1).getLink();
+
+					GroupElement U = this.grid.getU(index, j);
+					GroupElement Unext = this.grid.getUnext(index, j);
+					// U_x,i = V_x V_{x+i}^t
+					this.grid.setU(index, j, V0.mult(U).mult(V1.adj()));
+					this.grid.setUnext(index, j, V0next.mult(Unext).mult(V1next.adj()));
+
+					// Also write to copy of the grid.
+					this.gridCopy.setU(index, j, V0.mult(V1.adj()));
+					this.gridCopy.setUnext(index, j, V0next.mult(V1next.adj()));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Computes and stores the Gauss law violation at every lattice site.
+	 */
+	private class GaussViolationCalculation implements CellAction {
+		public AlgebraElement[] gaussViolation;
+		private ElementFactory factory;
+
+
+		public void reset(Grid grid) {
+			this.gaussViolation = new AlgebraElement[grid.getTotalNumberOfCells()];
+			this.factory = grid.getElementFactory();
+		}
+
+		public void execute(Grid grid, int index) {
+			if(grid.isActive(index)) {
+				this.gaussViolation[index] = grid.getGaussConstraint(index);
+			} else {
+				this.gaussViolation[index] = this.factory.algebraZero();
+			}
+		}
+
+		public AlgebraElement[] getResult() {
+			return gaussViolation;
+		}
+	}
+
+	/**
+	 * This class computes the electric fields from the gauge links (U and Unext).
+	 */
+	private class ElectricFieldSetter implements CellAction {
+		public void execute(Grid grid, int index) {
+			for (int j = 0; j < numberOfDimensions; j++) {
+				grid.setE(index, j, grid.getEFromLinks(index, j));
+			}
+		}
 	}
 }
