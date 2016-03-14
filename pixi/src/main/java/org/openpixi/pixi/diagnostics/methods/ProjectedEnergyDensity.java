4b3102021da82128669af64a4dac2538f8a555e4
David Müller <monolithu@gmail.com>
2016-03-14 15:29:01 +0100

    Added longitudinal poynting vector component to ProjectedEnergyDensity.

--- pixi/src/main/java/org/openpixi/pixi/diagnostics/methods/ProjectedEnergyDensity.java
+++ pixi/src/main/java/org/openpixi/pixi/diagnostics/methods/ProjectedEnergyDensity.java
@@ -2,6 +2,9 @@ package org.openpixi.pixi.diagnostics.methods;
 
 import org.openpixi.pixi.diagnostics.Diagnostics;
 import org.openpixi.pixi.diagnostics.FileFunctions;
+import org.openpixi.pixi.math.AlgebraElement;
+import org.openpixi.pixi.math.ElementFactory;
+import org.openpixi.pixi.math.GroupElement;
 import org.openpixi.pixi.parallel.cellaccess.CellAction;
 import org.openpixi.pixi.physics.Simulation;
 import org.openpixi.pixi.physics.grid.Grid;
@@ -10,7 +13,6 @@ import org.openpixi.pixi.physics.particles.IParticle;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -18,6 +20,7 @@ import java.util.ArrayList;
  * and writes 4 energy density components to a file:
  * Electric longitudinal (energyDensity_L_el), magnetic longitudinal (energyDensity_L_mag),
  * electric transversal  (energyDensity_T_el) and magnetic transversal (energyDensity_T_mag) energy density.
+ *
  * Using these four components one can compute various quantities:
  * total energy density = (sum of all components)
  * longitudinal energy density = (sum of longitudinal components)
@@ -25,12 +28,16 @@ import java.util.ArrayList;
  * longitudinal pressure = transversal energy density - longitudinal energy density
  * transvesal pressure = longitudinal energy density
  *
+ * It also outputs the longitudinal Poynting vector component (Averaged & Only time-averaged).
+ *
  * The output format:
  * 1) time
  * 2) electric transversal
  * 3) magnetic transversal
  * 4) electric longitudinal
  * 5) magnetic longitudinal
+ * 6) longitudinal poynting vector (averaged)
+ * 7) longitudinal poynting vector (only time-averaged)
  *
  */
 public class ProjectedEnergyDensity implements Diagnostics {
@@ -42,6 +49,7 @@ public class ProjectedEnergyDensity implements Diagnostics {
 	private int stepInterval;
 
 	private EnergyDensityComputation energyDensityComputation;
+	private PoyntingComputation poyntingComputation;
 
 	public ProjectedEnergyDensity(String path, double timeInterval, int direction) {
 		this.direction = direction;
@@ -53,6 +61,8 @@ public class ProjectedEnergyDensity implements Diagnostics {
 		this.stepInterval = (int) (timeInterval / s.getTimeStep());
 		this.energyDensityComputation = new EnergyDensityComputation();
 		energyDensityComputation.initialize(s.grid, direction);
+		this.poyntingComputation = new PoyntingComputation();
+		poyntingComputation.initialize(s.grid, direction);
 
 		FileFunctions.clearFile("output/" + path);
 	}
@@ -62,6 +72,11 @@ public class ProjectedEnergyDensity implements Diagnostics {
 			energyDensityComputation.reset();
 			grid.getCellIterator().execute(grid, energyDensityComputation);
 			energyDensityComputation.convertToEnergyUnits(grid);
+
+			poyntingComputation.reset();
+			grid.getCellIterator().execute(grid, poyntingComputation);
+			poyntingComputation.convertToEnergyUnits(grid);
+
 
 			// Write to file
 			File file = FileFunctions.getFile("output/" + path);
@@ -73,6 +88,8 @@ public class ProjectedEnergyDensity implements Diagnostics {
 				pw.write(FileFunctions.generateTSVString(energyDensityComputation.energyDensity_T_mag) + "\n");
 				pw.write(FileFunctions.generateTSVString(energyDensityComputation.energyDensity_L_el) + "\n");
 				pw.write(FileFunctions.generateTSVString(energyDensityComputation.energyDensity_L_mag) + "\n");
+				pw.write(FileFunctions.generateTSVString(poyntingComputation.poyntingAveraged) + "\n");
+				pw.write(FileFunctions.generateTSVString(poyntingComputation.poyntingTimeAveraged) + "\n");
 				pw.close();
 			} catch (IOException ex) {
 				System.out.println("ProjectedEnergyDensity: Error writing to file.");
@@ -148,6 +165,113 @@ public class ProjectedEnergyDensity implements Diagnostics {
 					energyDensity_L_el[projIndex] += e_L_el;
 					energyDensity_L_mag[projIndex] += e_L_mag;
 				}
+			}
+		}
+	}
+
+
+	private class PoyntingComputation implements CellAction {
+
+		private int direction;
+		private int numberOfDimensions;
+		private int numberOfCells;
+		private double[] poyntingAveraged;
+		private double[] poyntingTimeAveraged;
+		private double as;
+		private ElementFactory factory;
+
+		public void initialize(Grid grid, int direction) {
+			this.direction = direction;
+			this.numberOfCells = grid.getNumCells(direction);
+			this.poyntingAveraged = new double[numberOfCells];
+			this.poyntingTimeAveraged = new double[numberOfCells];
+			this.numberOfDimensions = grid.getNumberOfDimensions();
+			this.as = grid.getLatticeSpacing();
+			this.factory = grid.getElementFactory();
+		}
+
+		public void reset() {
+			for (int i = 0; i < numberOfCells; i++) {
+				this.poyntingAveraged[i] = 0.0;
+				this.poyntingTimeAveraged[i] = 0.0;
+			}
+		}
+
+		public void convertToEnergyUnits(Grid grid) {
+			// Divide by g*a factor
+			double invga = 1.0 / Math.pow(grid.getLatticeSpacing() * grid.getGaugeCoupling(), 2);
+
+			for (int i = 0; i < numberOfCells; i++) {
+				poyntingAveraged[i] *= invga;
+				poyntingTimeAveraged[i] *= invga;
+			}
+		}
+
+		/**
+		 * Computes the longitudinal Poynting vector component in 3D.
+		 * @param grid   Reference to the grid.
+		 * @param index	 Cell index.
+		 */
+		public void execute(Grid grid, int index) {
+			double localPoyntingAveraged = 0.0;
+			double localPoyntingTimeAveraged = 0.0;
+
+			int d = direction;
+			for (int i = 0; i < numberOfDimensions; i++) {
+				if(i != direction) {
+					// AVERAGED COMPUTATION
+
+					// Average spatial components of field strength tensor in space and time (B-Field).
+					GroupElement FG = factory.groupZero();
+
+					// Spatial average at t-at/2
+					FG.addAssign(grid.getPlaquette(index, d, i, 1, 1, 0));
+
+					FG.addAssign(grid.getPlaquette(index, i, d, -1, 1, 0));
+					FG.addAssign(grid.getPlaquette(index, i, d, 1, -1, 0));
+					FG.addAssign(grid.getPlaquette(index, d, i, -1, -1, 0));
+
+					// Spatial average at t+at/2
+					FG.addAssign(grid.getPlaquette(index, d, i, 1, 1, 1));
+					FG.addAssign(grid.getPlaquette(index, i, d, -1, 1, 1));
+					FG.addAssign(grid.getPlaquette(index, i, d, 1, -1, 1));
+					FG.addAssign(grid.getPlaquette(index, d, i, -1, -1, 1));
+
+					// Divide by factors and convert to AlgebraElement.
+					AlgebraElement F = FG.proj().mult(1.0 / (8.0 * as));
+
+					// Average E field in space.
+					int shiftedIndex = grid.shift(index, i, -1);
+					AlgebraElement E1 = grid.getE(index, i);
+					AlgebraElement E2 = grid.getE(shiftedIndex, i).act(grid.getLink(index, i, -1, 0));
+					AlgebraElement E = E1.add(E2).mult(0.5);
+
+					// Multiply E and F;
+					localPoyntingAveraged += E.mult(F);
+				}
+			}
+
+			// TIME-AVERAGED COMPUTATION:
+
+			// Indices for cross product:
+			int dir1 = (direction + 1) % 3;
+			int dir2 = (direction + 2) % 3;
+
+			// fields at same time:
+			AlgebraElement E1 = grid.getE(index, dir1);
+			AlgebraElement E2 = grid.getE(index, dir2);
+			// time averaged B-field:
+			AlgebraElement B1 = grid.getB(index, dir1, 0).add(grid.getB(index, dir1, 1)).mult(0.5);
+			AlgebraElement B2 = grid.getB(index, dir2, 0).add(grid.getB(index, dir2, 1)).mult(0.5);
+
+			localPoyntingTimeAveraged = E1.mult(B2) - E2.mult(B1);
+
+			// Add to array.
+			int projIndex = grid.getCellPos(index)[direction];
+			synchronized (this) {
+				this.poyntingAveraged[projIndex] += localPoyntingAveraged;
+				this.poyntingTimeAveraged[projIndex] += localPoyntingTimeAveraged;
+
 			}
 		}
 	}
