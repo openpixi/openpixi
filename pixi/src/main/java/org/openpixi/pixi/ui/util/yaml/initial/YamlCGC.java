f2c742a91bfa2f71741be083758b02d695bc8a61
Daniil Gelfand <gelfand@jpsi>
2016-07-28 15:00:54 +0200

    YAML support for the new nucleus initial conditions added. A first YAML file for the coherent version has been provided.

--- pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/initial/YamlCGC.java
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/initial/YamlCGC.java
@@ -1,13 +1,14 @@
 package org.openpixi.pixi.ui.util.yaml.initial;
 
 import org.openpixi.pixi.physics.Settings;
-import org.openpixi.pixi.ui.util.yaml.fieldgenerators.YamlSU2PlaneWave;
 
 import java.util.ArrayList;
 
 public class YamlCGC {
 	public ArrayList<YamlMVModel> MVModel = new ArrayList<YamlMVModel>();
 	public ArrayList<YamlMVModelCoherent> MVModelCoherent = new ArrayList<YamlMVModelCoherent>();
+	public ArrayList<YamlNucleusCoherent> NucleusCoherent = new ArrayList<YamlNucleusCoherent>();
+	public ArrayList<YamlNucleus> Nucleus = new ArrayList<YamlNucleus>();
 
 
 	/**
@@ -20,6 +21,14 @@ public class YamlCGC {
 		}
 
 		for (YamlMVModelCoherent init : MVModelCoherent) {
+			s.addInitialConditions(init.getInitialCondition());
+		}
+
+		for (YamlNucleusCoherent init : NucleusCoherent) {
+			s.addInitialConditions(init.getInitialCondition());
+		}
+
+		for (YamlNucleus init : Nucleus) {
 			s.addInitialConditions(init.getInitialCondition());
 		}
 	}
