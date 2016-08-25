a7c2a1decf0e4672d487d650f4474944fd8402ff
David Müller <david.mueller@tuwien.ac.at>
2016-08-26 00:11:21 +0200

    Updated FieldMeasurements to use new lattice units.

--- pixi/src/main/java/org/openpixi/pixi/physics/measurements/FieldMeasurements.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/measurements/FieldMeasurements.java
@@ -24,25 +24,25 @@ public class FieldMeasurements {
 	}
 
 	public double calculateEsquared(Grid grid) {
-		Esquared.reset();
+		Esquared.reset(grid);
 		grid.getCellIterator().execute(grid, Esquared);
         return Esquared.getSum(grid);
 	}
 	
 	public double calculateBsquared(Grid grid) {
-		Bsquared.reset();
+		Bsquared.reset(grid);
 		grid.getCellIterator().execute(grid, Bsquared);
         return Bsquared.getSum(grid);
 	}
 	
 	public double calculateEsquared(Grid grid, int dir) {
-		Esquared.reset();
+		Esquared.reset(grid);
 		grid.getCellIterator().execute(grid, Esquared);
         return Esquared.getSum(grid, dir);
 	}
 	
 	public double calculateBsquared(Grid grid, int dir) {
-		Bsquared.reset();
+		Bsquared.reset(grid);
 		grid.getCellIterator().execute(grid, Bsquared);
         return Bsquared.getSum(grid, dir);
 	}
@@ -66,21 +66,30 @@ public class FieldMeasurements {
 	}
 
 	private class EFieldSquared implements CellAction {
+		private double[] values;
+		private double[] unitFactors;
 
-		private double[] sum;
+        public void reset(Grid g)
+		{
+        	values = new double[g.getNumberOfDimensions()];
+			unitFactors = new double[g.getNumberOfDimensions()];
 
-        public void reset() {
-        	sum = new double[3];//TODO Make this method d-dimensional!!
-        }
+			for (int i = 0; i < g.getNumberOfDimensions(); i++) {
+				values[i] = 0.0;
+				unitFactors[i] = Math.pow(g.getLatticeUnitFactor(i), -2);
+			}
+		}
         
         public double getSum(Grid grid) {
-			double norm = Math.pow(grid.getLatticeSpacing()*grid.getGaugeCoupling(), 2) * grid.getTotalNumberOfCells();
-			return (sum[0]+sum[1]+sum[2]) / norm;
+        	double sum = 0.0;
+			for (int i = 0; i < values.length; i++) {
+				sum += values[i];
+			}
+			return sum;
         }
         
         public double getSum(Grid grid, int dir) {
-			double norm = Math.pow(grid.getLatticeSpacing()*grid.getGaugeCoupling(), 2) * grid.getTotalNumberOfCells();
-        	return sum[dir] / norm;
+        	return values[dir];
         }
 
         public void execute(Grid grid, int index) {
@@ -88,12 +97,13 @@ public class FieldMeasurements {
 				int numDir = grid.getNumberOfDimensions();
 				double[] res = new double[numDir];
 				for (int i = 0; i < numDir; i++) {
-					res[i] += grid.getE(index, i).square();
-					//res += grid.getEsquaredFromLinks(coor, i);
+					res[i] += grid.getE(index, i).square() * unitFactors[i];
 				}
+
+				// Synchronized summation.
 				synchronized (this) {
 					for (int i = 0; i < numDir; i++) {
-						sum[i] += res[i];    // Synchronisierte Summenbildung
+						values[i] += res[i];
 					}
 				}
 			}
@@ -101,43 +111,50 @@ public class FieldMeasurements {
 	}
 
 	private class BFieldSquared implements CellAction {
+		private double[] values;
+		private double[] unitFactors;
 
-		private double[] sum;
-
-        public void reset() {
-        	sum = new double[3];
-        }
-        
-        public double getSum(Grid grid)
+		public void reset(Grid g)
 		{
-			double norm = Math.pow(grid.getLatticeSpacing()*grid.getGaugeCoupling(), 2) * grid.getTotalNumberOfCells();
-			return (sum[0]+sum[1]+sum[2]) / norm;
-        }
+			values = new double[g.getNumberOfDimensions()];
+			unitFactors = new double[g.getNumberOfDimensions()];
+
+			for (int i = 0; i < g.getNumberOfDimensions(); i++) {
+				values[i] = 0.0;
+				unitFactors[i] = Math.pow(g.getLatticeUnitFactor(i), -2);
+			}
+		}
+
+		public double getSum(Grid grid) {
+			double sum = 0.0;
+			for (int i = 0; i < values.length; i++) {
+				sum += values[i];
+			}
+			return sum;
+		}
 
 		public double getSum(Grid grid, int dir) {
-			double norm = Math.pow(grid.getLatticeSpacing()*grid.getGaugeCoupling(), 2) * grid.getTotalNumberOfCells();
-			return sum[dir] / norm;
+			return values[dir];
 		}
-        
-        public void execute(Grid grid, int index) {
+
+		public void execute(Grid grid, int index) {
 			if(grid.isEvaluatable(index)) {
 				int numDir = grid.getNumberOfDimensions();
 				double[] res = new double[numDir];
 				for (int i = 0; i < numDir; i++) {
-					//res += grid.getB(coor, i).square();
-					// Averaging B(-dt/2) and B(dt/2) to approximate B(0).
-					res[i] += 0.5 * (grid.getBsquaredFromLinks(index, i, 0) + grid.getBsquaredFromLinks(index, i, 1));
+					res[i] += 0.5 * (grid.getB(index, i, 0).square() + grid.getB(index, i, 1).square()) * unitFactors[i];
 				}
 
+				// Synchronized summation.
 				synchronized (this) {
 					for (int i = 0; i < numDir; i++) {
-						sum[i] += res[i];    // Synchronisierte Summenbildung
+						values[i] += res[i];
 					}
 				}
 			}
 		}
 	}
-	
+
 	private class GaussLaw implements CellAction {
 
 		private double sum;
@@ -147,8 +164,7 @@ public class FieldMeasurements {
         }
         
         public double getSum(Grid grid) {
-			double norm = Math.pow(grid.getLatticeSpacing()*grid.getGaugeCoupling(), 2) * grid.getTotalNumberOfCells();
-			return sum / norm;
+			return sum;
         }
         
         public void execute(Grid grid, int index) {
@@ -170,9 +186,7 @@ public class FieldMeasurements {
 		}
 
 		public double getSum(Grid grid) {
-			charge.multAssign(Math.pow(grid.getLatticeSpacing(), grid.getNumberOfDimensions()));
-			double latticeUnitsNorm = grid.getGaugeCoupling() * grid.getLatticeSpacing();
-			return Math.sqrt(charge.square()) / latticeUnitsNorm;
+			return Math.sqrt(charge.square());
 		}
 
 		public void execute(Grid grid, int index) {
@@ -193,9 +207,7 @@ public class FieldMeasurements {
 		}
 
 		public double getSum(Grid grid) {
-			charge *= (Math.pow(grid.getLatticeSpacing(), 2*grid.getNumberOfDimensions()));
-			double latticeUnitsNorm = grid.getGaugeCoupling() * grid.getLatticeSpacing();
-			return Math.sqrt(charge) / latticeUnitsNorm;
+			return Math.sqrt(charge);
 		}
 
 		public void execute(Grid grid, int index) {
