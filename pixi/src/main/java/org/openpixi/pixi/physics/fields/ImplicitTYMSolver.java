5a50255d4757ed91b889050ae766f4bc08d67b26
Andreas Ipp <andreas.ipp@gmail.com>
2017-04-11 03:09:54 +0200

    Split CombinedUpdate into ImplicitBegin and ImplicitEnd.

--- pixi/src/main/java/org/openpixi/pixi/physics/fields/ImplicitTYMSolver.java
+++ pixi/src/main/java/org/openpixi/pixi/physics/fields/ImplicitTYMSolver.java
@@ -9,7 +9,8 @@ public class ImplicitTYMSolver extends FieldSolver
 
 	private double timeStep;
 	private UpdateLinks linkUpdater = new UpdateLinks();
-	private CombinedUpdate combinedUpdate = new CombinedUpdate();
+	private ImplicitBegin implicitBegin = new ImplicitBegin();
+	private ImplicitEnd implicitEnd = new ImplicitEnd();
 
 	@Override
 	public FieldSolver clone() {
@@ -17,18 +18,24 @@ public class ImplicitTYMSolver extends FieldSolver
 		clone.copyBaseClassFields(this);
 		clone.timeStep = timeStep;
 		clone.linkUpdater = linkUpdater;
-		clone.combinedUpdate = combinedUpdate;
+		clone.implicitBegin = implicitBegin;
+		clone.implicitEnd = implicitEnd;
 		return clone;
 	}
 
 	@Override
 	public void step(Grid grid, double timeStep) {
-		combinedUpdate.at = timeStep;
-		combinedUpdate.unitFactor = new double[grid.getNumberOfDimensions()];
+		Grid implicitGrid = new Grid(grid); // Copy grid.
+		implicitBegin.at = timeStep;
+		implicitBegin.unitFactor = new double[grid.getNumberOfDimensions()];
 		for (int i = 0; i < grid.getNumberOfDimensions(); i++) {
-			combinedUpdate.unitFactor[i] =  - grid.getLatticeUnitFactor(i) * grid.getTemporalSpacing();
+			implicitBegin.unitFactor[i] =  - grid.getLatticeUnitFactor(i) * grid.getTemporalSpacing();
 		}
-		cellIterator.execute(grid, combinedUpdate);
+		implicitBegin.implicitGrid = implicitGrid;
+		cellIterator.execute(grid, implicitBegin);
+
+		implicitEnd.implicitGrid = implicitGrid;
+		cellIterator.execute(grid, implicitEnd);
 	}
 
 	@Override
@@ -37,10 +44,11 @@ public class ImplicitTYMSolver extends FieldSolver
 		cellIterator.execute(grid, linkUpdater);
 	}
 
-	private class CombinedUpdate implements CellAction {
+	private class ImplicitBegin implements CellAction {
 
 		private double at;
 		private double[] unitFactor;
+		private Grid implicitGrid;
 
 		/**
 		 * Combined update of fields and links using the sum of staples.
@@ -52,11 +60,30 @@ public class ImplicitTYMSolver extends FieldSolver
 				GroupElement V;
 				for (int i = 0; i < grid.getNumberOfDimensions(); i++) {
 					GroupElement temp = grid.getU(index, i).mult(grid.getStapleSum(index, i));
-					grid.addE(index, i, temp.proj().mult(at)); // area factors already included in getStapleSum()
-					grid.addE(index, i, grid.getJ(index, i).mult(unitFactor[i]));
-					V = grid.getE(index, i).mult(-at).getLink();
+					implicitGrid.addE(index, i, temp.proj().mult(at)); // area factors already included in getStapleSum()
+					implicitGrid.addE(index, i, grid.getJ(index, i).mult(unitFactor[i]));
+					V = implicitGrid.getE(index, i).mult(-at).getLink();
 					V.multAssign(grid.getU(index, i));
-					grid.setUnext(index, i, V);
+					implicitGrid.setUnext(index, i, V);
+				}
+			}
+		}
+	}
+
+	private class ImplicitEnd implements CellAction {
+
+		private Grid implicitGrid;
+
+		/**
+		 * Combined update of fields and links using the sum of staples.
+		 * @param grid
+		 * @param index
+		 */
+		public void execute(Grid grid, int index) {
+			if(grid.isActive(index)) {
+				for (int i = 0; i < grid.getNumberOfDimensions(); i++) {
+					grid.setE(index, i, implicitGrid.getE(index, i));
+					grid.setUnext(index, i, implicitGrid.getUnext(index, i));
 				}
 			}
 		}
