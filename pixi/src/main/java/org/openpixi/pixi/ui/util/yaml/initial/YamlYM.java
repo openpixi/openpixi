9c6c38e426dafa6b37aa91cf32402ad939c1c17c
Andreas Ipp <andreas.ipp@gmail.com>
2017-04-19 16:00:59 +0200

    Add SU2PlanePulse to YAML InitialConditions specification

--- pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/initial/YamlYM.java
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/initial/YamlYM.java
@@ -9,10 +9,18 @@ public class YamlYM {
 
 	public ArrayList<YamlSU2PlaneWave> SU2PlaneWaves = new ArrayList<YamlSU2PlaneWave>();
 
+	public ArrayList<YamlSU2PlanePulse> SU2PlanePulses = new ArrayList<YamlSU2PlanePulse>();
+
 	public void applyTo(Settings s) {
 		for (YamlSU2PlaneWave wave : SU2PlaneWaves) {
 			if (wave.checkConsistency(s)) {
 				s.addInitialConditions(wave.getFieldGenerator());
+			}
+		}
+
+		for (YamlSU2PlanePulse pulse : SU2PlanePulses) {
+			if (pulse.checkConsistency(s)) {
+				s.addInitialConditions(pulse.getFieldGenerator());
 			}
 		}
 	}
