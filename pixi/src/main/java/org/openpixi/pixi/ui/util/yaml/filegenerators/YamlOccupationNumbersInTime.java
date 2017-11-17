eb4c5c5768ff08546a3246f4796139d22a7d5fc5
Andreas Ipp <andreas.ipp@gmail.com>
2017-11-17 15:57:15 +0100

    Add options to YamlOccupationNumbersInTime.

--- pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/filegenerators/YamlOccupationNumbersInTime.java
+++ pixi/src/main/java/org/openpixi/pixi/ui/util/yaml/filegenerators/YamlOccupationNumbersInTime.java
@@ -1,5 +1,6 @@
 package org.openpixi.pixi.ui.util.yaml.filegenerators;
 import org.openpixi.pixi.diagnostics.methods.OccupationNumbersInTime;
+import java.util.List;
 
 /**
  * Yaml wrapper for the YamlParticlesInTime FileGenerator.
@@ -27,6 +28,16 @@ public class YamlOccupationNumbersInTime {
 	 */
 	public Boolean colorful = false;
 
+	public Boolean useMirroredGrid;
+	public Integer mirroredDirection;
+	public Boolean useRectangularWindow;
+	public Double collisionTime;
+	public List<Double> collisionPosition;
+	public List<Double> coneVelocity;
+	public Boolean useGaussianWindow;
+	public Boolean useTukeyWindow;
+	public Double tukeyWidth;
+
 	/**
 	 * Returns an instance of CoulombGaugeInTime according to the parameters in the YAML file.
 	 *
@@ -34,11 +45,29 @@ public class YamlOccupationNumbersInTime {
 	 */
 	public OccupationNumbersInTime getFileGenerator() {
 		OccupationNumbersInTime fileGen;
-		if(colorful != null) {
+		if (useMirroredGrid != null) {
+
+			double[] collisionPositionArray = getDoubleArray(collisionPosition);
+			double[] coneVelocityArray = getDoubleArray(coneVelocity);
+
+			fileGen = new OccupationNumbersInTime(interval, outputType, path, colorful,
+					useMirroredGrid, mirroredDirection,
+					useRectangularWindow, collisionTime, collisionPositionArray, coneVelocityArray,
+					useGaussianWindow,
+					useTukeyWindow, tukeyWidth);
+		} else if(colorful != null) {
 			fileGen = new OccupationNumbersInTime(interval, outputType, path, colorful);
 		} else {
 			fileGen = new OccupationNumbersInTime(interval, outputType, path, false);
 		}
 		return fileGen;
+	}
+
+	private double[] getDoubleArray(List<Double> list) {
+		double[] array = new double[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			array[i] = list.get(i);
+		}
+		return array;
 	}
 }
